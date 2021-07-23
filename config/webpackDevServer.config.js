9f8e863f2ceea7ba1936b002a7c1b41fb404bb8c
Edwin van de Ven <edwin@simplycode.nl>
2021-07-23 14:11:25 +0200

    Proxy config for webback dev server

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -101,7 +101,11 @@ module.exports = function (proxy, allowedHost) {
     },
     public: allowedHost,
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
-    proxy,
+    // proxy,
+    proxy: [{
+        context: ['/d'],
+        target: 'http://localhost:8000'
+    }],
     before(app, server) {
       // Keep `evalSourceMapMiddleware` and `errorOverlayMiddleware`
       // middlewares before `redirectServedPath` otherwise will not have any effect
