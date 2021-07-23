a118cab6efe2feb614466563a6d59b003c3e2bf2
Edwin van de Ven <edwin@simplycode.nl>
2021-07-23 15:30:39 +0200

    Update HasPermissionWrapper to functional component

--- src/components/HasPermissionWrapper.js
+++ src/components/HasPermissionWrapper.js
@@ -1,51 +1,79 @@
 import React, { Component } from 'react'
-import { Query } from "@apollo/client"
+import { useQuery, Query } from "@apollo/client"
 
 import GET_USER from '../queries/system/get_user'
 import { get_all_permissions } from "../tools/user_tools"
 
-class HasPermissionWrapper extends Component {
-  constructor(props) {
-    super(props)
-    // console.log("HasPermissionWrapper props:")
-    // console.log(props)
-  }
 
-  check_permission(permissions, permission, resource) {
-    let you_shall_not_pass = true
+function check_permission(permissions, permission, resource) {
+  let you_shall_not_pass = true
 
-    if (resource in permissions) {
-      if (permissions[resource].has(permission)) {
-        you_shall_not_pass = false
-      }
+  if (resource in permissions) {
+    if (permissions[resource].has(permission)) {
+      you_shall_not_pass = false
     }
-    
-    return !you_shall_not_pass
   }
   
+  return !you_shall_not_pass
+}
 
-  render() {
-    return (
-      <Query query={GET_USER} >
+function HasPermissionWrapper({permission, resource, children}) {
+  const {loading, error, data} = useQuery(GET_USER)
+  
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error loading user... :(</p>
 
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>
-          if (error) return <p>Error loading user... :(</p>
-          
-          // console.log(data)
-          const permissions = get_all_permissions(data.user)
-          // console.log('permissions:')
-          // console.log(permissions)
+  const permissions = get_all_permissions(data.user)
 
-          if (this.check_permission(permissions, this.props.permission, this.props.resource)) {
-            return this.props.children
-          } else {
-            return ''
-          }
-        }}
-
-      </Query>
-    )}
+  if (check_permission(permissions, permission, resource)) {
+    return children
+  } else {
+    return ''
   }
+}
+
+// class HasPermissionWrapper extends Component {
+//   constructor(props) {
+//     super(props)
+//     // console.log("HasPermissionWrapper props:")
+//     // console.log(props)
+//   }
+
+  
+//   check_permission(permissions, permission, resource) {
+//     let you_shall_not_pass = true
+
+//     if (resource in permissions) {
+//       if (permissions[resource].has(permission)) {
+//         you_shall_not_pass = false
+//       }
+//     }
+    
+//     return !you_shall_not_pass
+//   }
+
+//   render() {
+//     return (
+//       <Query query={GET_USER} >
+
+//         {({ loading, error, data }) => {
+//           if (loading) return <p>Loading...</p>
+//           if (error) return <p>Error loading user... :(</p>
+          
+//           // console.log(data)
+//           const permissions = get_all_permissions(data.user)
+//           // console.log('permissions:')
+//           // console.log(permissions)
+
+//           if (this.check_permission(permissions, this.props.permission, this.props.resource)) {
+//             return this.props.children
+//           } else {
+//             return ''
+//           }
+//         }}
+
+//       </Query>
+//     )}
+//   }
   
   export default HasPermissionWrapper
