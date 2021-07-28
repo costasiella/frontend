d7e4b6c6bd59b7bf3e24f94b7b6865e28b8bb2e4
Edwin van de Ven <edwin@simplycode.nl>
2021-07-28 21:24:49 +0200

    Add RelationsProfileBase component

---
+++ src/components/relations/accounts/RelationsAccountProfileBase.jsx
@@ -0,0 +1,53 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import {
+  Page,
+  Grid,
+  Card,
+  Container
+} from "tabler-react"
+import SiteWrapper from "../../SiteWrapper"
+import HasPermissionWrapper from "../../HasPermissionWrapper"
+import ProfileCardSmall from "../../ui/ProfileCardSmall"
+
+import RelationsAccountsBack from "./RelationsAccountsBack"
+import ProfileMenu from "./ProfileMenu"
+
+function RelationsAccountProfileBase({t, match, children, headerTitle="", user={}}) {
+  const accountId = match.params.account_id
+
+  return (
+    <SiteWrapper>
+      <div className="my-3 my-md-5">
+        <Container>
+          <Page.Header title={headerTitle}>
+            <RelationsAccountsBack />
+          </Page.Header>
+          <Grid.Row>
+            <Grid.Col md={9}>
+            <Card>
+              <Card.Header>
+                <Card.Title>{t('relations.accounts.profile')}</Card.Title>
+              </Card.Header>
+              {children}
+            </Card>
+            </Grid.Col>                                    
+            <Grid.Col md={3}>
+              <ProfileCardSmall user={user}/>
+              <ProfileMenu 
+                active_link='profile'
+                accountId={accountId}
+              /> 
+            </Grid.Col>
+          </Grid.Row>
+        </Container>
+      </div>
+    </SiteWrapper>
+  )
+}
+
+export default withTranslation()(withRouter(RelationsAccountProfileBase))
