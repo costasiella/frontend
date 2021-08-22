3d9d2902fcbb4d155a535c3c09edd46e392bd0de
Edwin van de Ven <edwin@simplycode.nl>
2021-08-22 12:00:28 +0200

    Add FinanceCostCentersBase component

---
+++ src/components/finance/costcenters/FinanceCostCentersBase.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Link } from "react-router-dom"
+
+
+import {
+  Page,
+  Grid,
+  Icon,
+  Button,
+  Container,
+} from "tabler-react";
+import SiteWrapper from "../../SiteWrapper"
+import HasPermissionWrapper from "../../HasPermissionWrapper"
+
+import FinanceMenu from "../FinanceMenu"
+
+
+function FinanceCostCentersBase({t, history, children, showBack=false}) {
+  return (
+    <SiteWrapper>
+      <div className="my-3 my-md-5">
+        <Container>
+          <Page.Header title={t("finance.title")} />
+          <Grid.Row>
+            <Grid.Col md={9}>
+              {children}
+            </Grid.Col>
+            <Grid.Col md={3}>
+              {(showBack) ?
+                <Link to={"/finance/costcenters"}>
+                  <Button color="primary btn-block mb-6">
+                    <Icon prefix="fe" name="chevrons-left" /> {t('general.back')}
+                  </Button>
+                </Link>
+                :
+                <HasPermissionWrapper permission="add"
+                                      resource="financeglaccount">
+                  <Link to={"/finance/costcenters/add"}>
+                    <Button color="primary btn-block mb-6">
+                      <Icon prefix="fe" name="plus-circle" /> {t('finance.costcenters.add')}
+                    </Button>
+                  </Link>
+                </HasPermissionWrapper>
+              }
+              <FinanceMenu activeLink='costcenters'/>
+            </Grid.Col>
+          </Grid.Row>
+        </Container>
+      </div>
+    </SiteWrapper>
+  )
+}
+
+
+export default withTranslation()(withRouter(FinanceCostCentersBase))
