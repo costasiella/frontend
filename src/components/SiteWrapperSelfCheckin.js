9f56174a8a72056cc2914cf9b8f38419d7c3ed43
Edwin van de Ven <edwin@simplycode.nl>
2021-08-31 16:36:58 +0200

    Functional component for SiteWrapperSelfCheckin

--- src/components/SiteWrapperSelfCheckin.js
+++ src/components/SiteWrapperSelfCheckin.js
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { withTranslation } from 'react-i18next'
 import { NavLink, withRouter } from "react-router-dom"
-import { Query } from "@apollo/client"
+import { useQuery } from "@apollo/client"
 import { ToastContainer, Slide } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import 'react-confirm-alert/src/react-confirm-alert.css'
@@ -122,139 +122,269 @@ const getNavBarItems = (t, user) => {
 
 const now = new Date()
 
-class SiteWrapperSelfCheckin extends React.Component<Props, State> {
-  state = {}  
 
-  render(): React.Node {
-    return (
-      <Query query={GET_USER} >
-        {({ loading, error, data }) => {
-          if (loading) return <p>{this.props.t('general.loading_with_dots')}</p>;
-          if (error) return <p>{this.props.t('system.user.error_loading')}</p>; 
-          
-          console.log('user data in site wrapper')
-          console.log(data)
-      
-          return <Site.Wrapper
-            headerProps={{
-                href: "/",
-                alt: "Costasiella",
-                imageURL: "/d/static/logos/stock/logo_stock_backend.svg", // Set logo url here
-                // navItems: (
-                //   <Nav.Item type="div" className="d-none d-md-flex">
-                //     <Link to="/settings/general">
-                //       <Button
-                //         icon="settings"
-                //         outline
-                //         size="sm"
-                //         color="primary"
-                //       >
-                //         {this.props.t('general.settings')}
-                //       </Button>
-                //     </Link>
-                //   </Nav.Item>
-                // ),
-                // notificationsTray: {
-                //   notificationsObjects,
-                //   markAllAsRead: () =>
-                //     this.setState(
-                //       () => ({
-                //         notificationsObjects: this.state.notificationsObjects.map(
-                //           v => ({ ...v, unread: false })
-                //         ),
-                //       }),
-                //       () =>
-                //         setTimeout(
-                //           () =>
-                //             this.setState({
-                //               notificationsObjects: this.state.notificationsObjects.map(
-                //                 v => ({ ...v, unread: true })
-                //               ),
-                //             }),
-                //           5000
-                //         )
-                //     ),
-                //   unread: unreadCount,
-                // },
-                accountDropdown: {
-                avatarURL: "#",
-                name: data.user.firstName + ' ' + data.user.lastName,
-                description: "",
-                options: [
-                  // { icon: "user", value: "Profile" },
-                  { icon: "lock", value: "Change password", to: "/#/user/password/change/" },
-                  { isDivider: true },
-                  { icon: "log-out", value: "Sign out", to: "/#/user/logout/" },
-                ],
-              },
-              }}
-              // navProps={{ itemsObjects: navBarItems }}
-              // navProps={{ itemsObjects: getNavBarItems(this.props.t, data.user) }}
-              routerContextComponentType={withRouter(RouterContextProvider)}
-              footerProps={{
-                // links: [
-                //   <a href="#">First Link</a>,
-                //   <a href="#">Second Link</a>,
-                //   <a href="#">Third Link</a>,
-                //   <a href="#">Fourth Link</a>,
-                //   <a href="#">Five Link</a>,
-                //   <a href="#">Sixth Link</a>,
-                //   <a href="#">Seventh Link</a>,
-                //   <a href="#">Eigth Link</a>,
-                // ],
-                // note:
-                //   "Premium and Open Source dashboard template with responsive and high quality UI. For Free!",
-                copyright: (
-                  <React.Fragment>
-                    Copyleft © {now.getFullYear()}.
-                    <a
-                      href="https://www.costasiella.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {" "}
-                      Edwin van de Ven
-                    </a>{". "}
-                    All rights reserved.
-                  </React.Fragment>
-                ),
-                nav: (
-                  <React.Fragment>
-                    <Grid.Col auto={true}>
-                      {/* <List className="list-inline list-inline-dots mb-0">
-                        <List.Item className="list-inline-item">
-                          <a href="./docs/index.html">Documentation</a>
-                        </List.Item>
-                        <List.Item className="list-inline-item">
-                          <a href="./faq.html">FAQ</a>
-                        </List.Item>
-                      </List> */}
-                    </Grid.Col>
-                    <Grid.Col auto={true}>
-                      {/* <Button
-                        href="https://github.com/tabler/tabler-react"
-                        size="sm"
-                        outline
-                        color="primary"
-                        RootComponent="a"
-                      >
-                        Source code
-                      </Button> */}
-                    </Grid.Col>
-                  </React.Fragment>
-                ),
-              }}
+function SiteWrapperSelfCheckin({t, match, history, children}) {
+  const { error, loading, data, fetchMore } = useQuery(GET_USER)
+
+  if (loading) return <p>{t('general.loading_with_dots')}</p>;
+  if (error) return <p>{t('system.user.error_loading')}</p>; 
+
+  console.log(data)
+
+  return (
+    <Site.Wrapper
+      headerProps={{
+        href: "/",
+        alt: "Costasiella",
+        imageURL: "/d/static/logos/stock/logo_stock_backend.svg", // Set logo url here
+        // navItems: (
+        //   <Nav.Item type="div" className="d-none d-md-flex">
+        //     <Link to="/settings/general">
+        //       <Button
+        //         icon="settings"
+        //         outline
+        //         size="sm"
+        //         color="primary"
+        //       >
+        //         {this.props.t('general.settings')}
+        //       </Button>
+        //     </Link>
+        //   </Nav.Item>
+        // ),
+        // notificationsTray: {
+        //   notificationsObjects,
+        //   markAllAsRead: () =>
+        //     this.setState(
+        //       () => ({
+        //         notificationsObjects: this.state.notificationsObjects.map(
+        //           v => ({ ...v, unread: false })
+        //         ),
+        //       }),
+        //       () =>
+        //         setTimeout(
+        //           () =>
+        //             this.setState({
+        //               notificationsObjects: this.state.notificationsObjects.map(
+        //                 v => ({ ...v, unread: true })
+        //               ),
+        //             }),
+        //           5000
+        //         )
+        //     ),
+        //   unread: unreadCount,
+        // },
+        accountDropdown: {
+        avatarURL: "#",
+        name: data.user.firstName + ' ' + data.user.lastName,
+        description: "",
+        options: [
+          // { icon: "user", value: "Profile" },
+          { icon: "lock", value: "Change password", to: "/#/user/password/change/" },
+          { isDivider: true },
+          { icon: "log-out", value: "Sign out", to: "/#/user/logout/" },
+        ],
+      },
+      }}
+      // navProps={{ itemsObjects: navBarItems }}
+      // navProps={{ itemsObjects: getNavBarItems(this.props.t, data.user) }}
+      routerContextComponentType={withRouter(RouterContextProvider)}
+      footerProps={{
+        // links: [
+        //   <a href="#">First Link</a>,
+        //   <a href="#">Second Link</a>,
+        //   <a href="#">Third Link</a>,
+        //   <a href="#">Fourth Link</a>,
+        //   <a href="#">Five Link</a>,
+        //   <a href="#">Sixth Link</a>,
+        //   <a href="#">Seventh Link</a>,
+        //   <a href="#">Eigth Link</a>,
+        // ],
+        // note:
+        //   "Premium and Open Source dashboard template with responsive and high quality UI. For Free!",
+        copyright: (
+          <React.Fragment>
+            Copyleft © {now.getFullYear()}.
+            <a
+              href="https://www.costasiella.com"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              {this.props.children}
-              <ToastContainer 
-                autoClose={5000} 
-                transition={Slide}
-              />
-            </Site.Wrapper>
-          }}
-        </Query>
-    );
-  }
+              {" "}
+              Edwin van de Ven
+            </a>{". "}
+            All rights reserved.
+          </React.Fragment>
+        ),
+        nav: (
+          <React.Fragment>
+            <Grid.Col auto={true}>
+              {/* <List className="list-inline list-inline-dots mb-0">
+                <List.Item className="list-inline-item">
+                  <a href="./docs/index.html">Documentation</a>
+                </List.Item>
+                <List.Item className="list-inline-item">
+                  <a href="./faq.html">FAQ</a>
+                </List.Item>
+              </List> */}
+            </Grid.Col>
+            <Grid.Col auto={true}>
+              {/* <Button
+                href="https://github.com/tabler/tabler-react"
+                size="sm"
+                outline
+                color="primary"
+                RootComponent="a"
+              >
+                Source code
+              </Button> */}
+            </Grid.Col>
+          </React.Fragment>
+        ),
+      }}
+    >
+      {children}
+      <ToastContainer 
+        autoClose={5000} 
+        transition={Slide}
+      />
+    </Site.Wrapper> 
+  )
 }
+
+
+// class SiteWrapperSelfCheckin extends React.Component<Props, State> {
+//   state = {}  
+
+//   render(): React.Node {
+//     return (
+//       <Query query={GET_USER} >
+//         {({ loading, error, data }) => {
+//           if (loading) return <p>{this.props.t('general.loading_with_dots')}</p>;
+//           if (error) return <p>{this.props.t('system.user.error_loading')}</p>; 
+          
+//           console.log('user data in site wrapper')
+//           console.log(data)
+      
+//           return <Site.Wrapper
+//             headerProps={{
+//                 href: "/",
+//                 alt: "Costasiella",
+//                 imageURL: "/d/static/logos/stock/logo_stock_backend.svg", // Set logo url here
+//                 // navItems: (
+//                 //   <Nav.Item type="div" className="d-none d-md-flex">
+//                 //     <Link to="/settings/general">
+//                 //       <Button
+//                 //         icon="settings"
+//                 //         outline
+//                 //         size="sm"
+//                 //         color="primary"
+//                 //       >
+//                 //         {this.props.t('general.settings')}
+//                 //       </Button>
+//                 //     </Link>
+//                 //   </Nav.Item>
+//                 // ),
+//                 // notificationsTray: {
+//                 //   notificationsObjects,
+//                 //   markAllAsRead: () =>
+//                 //     this.setState(
+//                 //       () => ({
+//                 //         notificationsObjects: this.state.notificationsObjects.map(
+//                 //           v => ({ ...v, unread: false })
+//                 //         ),
+//                 //       }),
+//                 //       () =>
+//                 //         setTimeout(
+//                 //           () =>
+//                 //             this.setState({
+//                 //               notificationsObjects: this.state.notificationsObjects.map(
+//                 //                 v => ({ ...v, unread: true })
+//                 //               ),
+//                 //             }),
+//                 //           5000
+//                 //         )
+//                 //     ),
+//                 //   unread: unreadCount,
+//                 // },
+//                 accountDropdown: {
+//                 avatarURL: "#",
+//                 name: data.user.firstName + ' ' + data.user.lastName,
+//                 description: "",
+//                 options: [
+//                   // { icon: "user", value: "Profile" },
+//                   { icon: "lock", value: "Change password", to: "/#/user/password/change/" },
+//                   { isDivider: true },
+//                   { icon: "log-out", value: "Sign out", to: "/#/user/logout/" },
+//                 ],
+//               },
+//               }}
+//               // navProps={{ itemsObjects: navBarItems }}
+//               // navProps={{ itemsObjects: getNavBarItems(this.props.t, data.user) }}
+//               routerContextComponentType={withRouter(RouterContextProvider)}
+//               footerProps={{
+//                 // links: [
+//                 //   <a href="#">First Link</a>,
+//                 //   <a href="#">Second Link</a>,
+//                 //   <a href="#">Third Link</a>,
+//                 //   <a href="#">Fourth Link</a>,
+//                 //   <a href="#">Five Link</a>,
+//                 //   <a href="#">Sixth Link</a>,
+//                 //   <a href="#">Seventh Link</a>,
+//                 //   <a href="#">Eigth Link</a>,
+//                 // ],
+//                 // note:
+//                 //   "Premium and Open Source dashboard template with responsive and high quality UI. For Free!",
+//                 copyright: (
+//                   <React.Fragment>
+//                     Copyleft © {now.getFullYear()}.
+//                     <a
+//                       href="https://www.costasiella.com"
+//                       target="_blank"
+//                       rel="noopener noreferrer"
+//                     >
+//                       {" "}
+//                       Edwin van de Ven
+//                     </a>{". "}
+//                     All rights reserved.
+//                   </React.Fragment>
+//                 ),
+//                 nav: (
+//                   <React.Fragment>
+//                     <Grid.Col auto={true}>
+//                       {/* <List className="list-inline list-inline-dots mb-0">
+//                         <List.Item className="list-inline-item">
+//                           <a href="./docs/index.html">Documentation</a>
+//                         </List.Item>
+//                         <List.Item className="list-inline-item">
+//                           <a href="./faq.html">FAQ</a>
+//                         </List.Item>
+//                       </List> */}
+//                     </Grid.Col>
+//                     <Grid.Col auto={true}>
+//                       {/* <Button
+//                         href="https://github.com/tabler/tabler-react"
+//                         size="sm"
+//                         outline
+//                         color="primary"
+//                         RootComponent="a"
+//                       >
+//                         Source code
+//                       </Button> */}
+//                     </Grid.Col>
+//                   </React.Fragment>
+//                 ),
+//               }}
+//             >
+//               {this.props.children}
+//               <ToastContainer 
+//                 autoClose={5000} 
+//                 transition={Slide}
+//               />
+//             </Site.Wrapper>
+//           }}
+//         </Query>
+//     );
+//   }
+// }
 
 export default withTranslation()(SiteWrapperSelfCheckin)
