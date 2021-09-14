9db8aa356f5c8b794fe5e4447d4ad2cfcff97c49
Edwin van de Ven <edwin@simplycode.nl>
2021-09-14 17:57:14 +0200

    Add holiday queries

---
+++ src/components/organization/holidays/queries.js
@@ -0,0 +1,74 @@
+import { gql } from "@apollo/client"
+
+export const GET_HOLIDAYS_QUERY = gql`
+  query OrganizationHolidays($after: String, $before: String) {
+    organizationHolidays(first: 15, before: $before, after: $after) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          name
+          description
+          dateStart
+          dateEnd
+          classes
+          organizationLocations {
+            edges {
+              node {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export const GET_HOLIDAY_QUERY = gql`
+  query OrganizationHoliday($id: ID!) {
+    organizationHoliday(id:$id) {
+      id
+      name
+      description
+      dateStart
+      dateEnd
+      classes
+    }
+  }
+`
+
+export const ADD_HOLIDAY = gql`
+mutation CreateOrganizationHoliday($input:CreateOrganizationHolidayInput!) {
+  createOrganizationHoliday(input: $input) {
+    organizationHoliday {
+      id
+    }
+  }
+}
+`
+
+export const UPDATE_HOLIDAY = gql`
+mutation UpdateOrganizationHoliday($input: UpdateOrganizationHolidayInput!) {
+  updateOrganizationHoliday(input: $input) {
+    organizationHoliday {
+      id
+      name
+    }
+  }
+}
+`
+
+export const DELETE_HOLIDAY = gql`
+mutation DeleteOrganizationHoliday($input: ArchiveOrganizationHolidayInput!) {
+  deleteOrganizationHoliday(input: $input) {
+    ok
+  }
+}
+`
