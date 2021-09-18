c0fa50ef72c5c71079b4475ec02c8e572a888c51
Edwin van de Ven <edwin@simplycode.nl>
2021-09-18 12:47:05 +0200

    Add route to Add Holiday component

--- src/AppRoot.js
+++ src/AppRoot.js
@@ -106,6 +106,7 @@ import OrganizationListDocuments from './components/organization/documents/Organ
 import OrganizationDocumentAdd from './components/organization/documents/OrganizationDocumentAdd'
 import OrganizationDocumentEdit from './components/organization/documents/OrganizationDocumentEdit'
 import OrganizationHolidays from './components/organization/holidays/OrganizationHolidays'
+import OrganizationHolidayAdd from './components/organization/holidays/OrganizationHolidayAdd'
 import OrganizationLanguages from './components/organization/languages/OrganizationLanguages'
 import OrganizationLanguageAdd from './components/organization/languages/OrganizationLanguageAdd'
 import OrganizationLanguageEdit from './components/organization/languages/OrganizationLanguageEdit'
@@ -468,6 +469,7 @@ function AppRoot({ t }) {
             <PrivateRoute exact path="/organization/discoveries/add" component={OrganizationDiscoveryAdd} /> 
             <PrivateRoute exact path="/organization/discoveries/edit/:id" component={OrganizationDiscoveryEdit} /> 
             <PrivateRoute exact path="/organization/holidays" component={OrganizationHolidays} />
+            <PrivateRoute exact path="/organization/holidays/add" component={OrganizationHolidayAdd} />
             <PrivateRoute exact path="/organization/languages" component={OrganizationLanguages} />
             <PrivateRoute exact path="/organization/languages/add" component={OrganizationLanguageAdd} />
             <PrivateRoute exact path="/organization/languages/edit/:id" component={OrganizationLanguageEdit} />
