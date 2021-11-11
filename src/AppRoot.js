39e8a5e73e39268fbf97a044fda7a75ba31dcf7b
Edwin van de Ven <edwin@simplycode.nl>
2021-11-11 14:30:33 +0100

    Add route to AccountDocumentEdit component

--- src/AppRoot.js
+++ src/AppRoot.js
@@ -154,6 +154,7 @@ import AccountClasspassAdd from './components/relations/accounts/classpasses/Acc
 import AccountClasspassEdit from './components/relations/accounts/classpasses/AccountClasspassEdit'
 import AccountDocuments from './components/relations/accounts/documents/AccountDocuments'
 import AccountDocumentAdd from './components/relations/accounts/documents/AccountDocumentAdd'
+import AccountDocumentEdit from './components/relations/accounts/documents/AccountDocumentEdit'
 import AccountFinancePaymentBatchCategoryItems from 
   './components/relations/accounts/finance_payment_batch_category_items/AccountFinancePaymentBatchCategoryItems'
 import AccountFinancePaymentBatchCategoryItemAdd from 
@@ -531,6 +532,7 @@ function AppRoot({ t }) {
             <PrivateRoute exact path="/relations/accounts/:account_id/classpasses/edit/:id" component={AccountClasspassEdit} />
             <PrivateRoute exact path="/relations/accounts/:account_id/documents" component={AccountDocuments} />
             <PrivateRoute exact path="/relations/accounts/:account_id/documents/add" component={AccountDocumentAdd} />
+            <PrivateRoute exact path="/relations/accounts/:account_id/documents/edit/:id" component={AccountDocumentEdit} />
             <PrivateRoute exact path="/relations/accounts/:account_id/finance_payment_batch_category_items" 
                           component={AccountFinancePaymentBatchCategoryItems} />
             <PrivateRoute exact path="/relations/accounts/:account_id/finance_payment_batch_category_items/add" 
