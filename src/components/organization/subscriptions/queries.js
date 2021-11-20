15ac01795c8fcf07a641db0bbb8421d60cd0547a
Edwin van de Ven <edwin@simplycode.nl>
2021-11-20 11:25:31 +0100

    Fixes #42

--- src/components/organization/subscriptions/queries.js
+++ src/components/organization/subscriptions/queries.js
@@ -24,7 +24,6 @@ export const GET_SUBSCRIPTIONS_QUERY = gql`
           subscriptionUnit
           subscriptionUnitDisplay
           reconciliationClasses
-          creditValidity
           unlimited
           termsAndConditions
           registrationFee
