b76a7d31674849738b83793d3ff1235e087b25ba
Edwin van de Ven <edwin@simplycode.nl>
2021-11-24 12:44:28 +0100

    Remove revenue menu

--- src/components/insight/home/InsightHome.jsx
+++ src/components/insight/home/InsightHome.jsx
@@ -3,22 +3,18 @@
 import React, {Component } from 'react'
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-import { Link } from "react-router-dom"
 
 import {
   Card,
   Page,
   Grid,
   Container,
-  StampCard
 } from "tabler-react";
 
 import HomeItemButton from "../../ui/HomeItemButton"
 import SiteWrapper from "../../SiteWrapper"
 import HasPermissionWrapper from "../../HasPermissionWrapper"
 
-
-import InsightMenu from "../InsightMenu"
 
 
 class InsightHome extends Component {
@@ -40,42 +36,35 @@ class InsightHome extends Component {
           <Container>
             <Page.Header title={t("insight.title")} />
             <Grid.Row>
-              <Grid.Col md={9}>
-                <Grid.Row>
-                  <Grid.Col md={4} lg={4}>
-                    <Card>
-                      <Card.Body>
-                        <h5>{t("insight.classpasses.title")}</h5>
-                        {t("insight.classpasses.explanation")}
-                        <br /><br />
-                        <HomeItemButton linkTitle={linkTitle} link="/insight/classpasses" />
-                      </Card.Body>
-                    </Card>
-                  </Grid.Col>
-                  <Grid.Col md={4} lg={4}>
-                    <Card>
-                      <Card.Body>
-                        <h5>{t("insight.subscriptions.title")}</h5>
-                        {t("insight.subscriptions.explanation")}
-                        <br /><br />
-                        <HomeItemButton linkTitle={linkTitle} link="/insight/subscriptions" />
-                      </Card.Body>
-                    </Card>
-                  </Grid.Col>
-                  <Grid.Col md={4} lg={4}>
-                    <Card>
-                      <Card.Body>
-                        <h5>{t("insight.revenue.title")}</h5>
-                        {t("insight.revenue.explanation")}
-                        <br /><br />
-                        <HomeItemButton linkTitle={linkTitle} link="/insight/revenue" />
-                      </Card.Body>
-                    </Card>
-                  </Grid.Col>
-                </Grid.Row>
+              <Grid.Col md={3} lg={3}>
+                <Card>
+                  <Card.Body>
+                    <h5>{t("insight.classpasses.title")}</h5>
+                    {t("insight.classpasses.explanation")}
+                    <br /><br />
+                    <HomeItemButton linkTitle={linkTitle} link="/insight/classpasses" />
+                  </Card.Body>
+                </Card>
               </Grid.Col>
-              <Grid.Col md={3}>
-                <InsightMenu />
+              <Grid.Col md={3} lg={3}>
+                <Card>
+                  <Card.Body>
+                    <h5>{t("insight.subscriptions.title")}</h5>
+                    {t("insight.subscriptions.explanation")}
+                    <br /><br />
+                    <HomeItemButton linkTitle={linkTitle} link="/insight/subscriptions" />
+                  </Card.Body>
+                </Card>
+              </Grid.Col>
+              <Grid.Col md={3} lg={3}>
+                <Card>
+                  <Card.Body>
+                    <h5>{t("insight.revenue.title")}</h5>
+                    {t("insight.revenue.explanation")}
+                    <br /><br />
+                    <HomeItemButton linkTitle={linkTitle} link="/insight/revenue" />
+                  </Card.Body>
+                </Card>
               </Grid.Col>
             </Grid.Row>
           </Container>
