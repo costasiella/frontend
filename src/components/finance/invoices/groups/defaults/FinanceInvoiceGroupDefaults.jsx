5d59b9269ba728711c6724dc0f2a1e0752daedfc
Edwin van de Ven <edwin@simplycode.nl>
2021-12-06 09:03:49 +0100

    Functional component for FinanceInvoiceGroupDefaults

--- src/components/finance/invoices/groups/defaults/FinanceInvoiceGroupDefaults.jsx
+++ src/components/finance/invoices/groups/defaults/FinanceInvoiceGroupDefaults.jsx
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { Query, Mutation } from "@apollo/client"
+import { useQuery, useMutation } from "@apollo/client"
 import { gql } from "@apollo/client"
 import { v4 } from "uuid"
 import { Formik } from 'formik'
@@ -15,12 +15,8 @@ import {
   Grid,
   Icon,
   Dimmer,
-  Badge,
-  Button,
-  Card,
   Container,
   Table,
-  Text
 } from "tabler-react";
 import SiteWrapper from "../../../../SiteWrapper"
 import HasPermissionWrapper from "../../../../HasPermissionWrapper"
@@ -29,27 +25,12 @@ import { toast } from 'react-toastify'
 
 import ContentCard from "../../../../general/ContentCard"
 import FinanceMenu from "../../../FinanceMenu"
+import FinanceInvoiceGroupDefaultsBase from './FinanceInvoiceGroupDefaultsBase'
 import FinanceInvoiceGroupDefaultForm from './FinanceInvoiceGroupDefaultForm'
 
 import { INVOICE_GROUP_DEFAULT_SCHEMA } from "./yupSchema"
 import { GET_INVOICE_GROUPS_QUERY } from "../queries"
-import { GET_INVOICE_GROUPS_DEFAULTS_QUERY } from "./queries"
-
-
-const UPDATE_INVOICE_GROUP_DEFAULT = gql`
-  mutation UpdateFinanceInvoiceGroupDefault($input: UpdateFinanceInvoiceGroupDefaultInput!) {
-    updateFinanceInvoiceGroupDefault(input: $input) {
-      financeInvoiceGroupDefault {
-        id
-        itemType
-        financeInvoiceGroup {
-          id
-          name
-        }
-      }
-    }
-  }
-`
+import { GET_INVOICE_GROUPS_DEFAULTS_QUERY, UPDATE_INVOICE_GROUP_DEFAULT } from "./queries"
 
 
 const fetch_default_type_name = (t, itemType) => {
@@ -88,165 +69,138 @@ const fetch_default_type_name = (t, itemType) => {
 
 
 
-const FinanceInvoiceGroupsDefaults = ({ t, history }) => (
-  <SiteWrapper>
-    <div className="my-3 my-md-5">
-      <Container>
-        <Page.Header title={t("finance.title")}>
-          <div className="page-options d-flex">
-            <Link to="/finance/invoices/groups" 
-                  className='btn btn-outline-secondary btn-sm'>
-                <Icon prefix="fe" name="arrow-left" /> {t('general.back_to')} {t('finance.invoice_groups.title')}
-            </Link>
-          </div>
-        </Page.Header>
-        <Grid.Row>
-          <Grid.Col md={9}>
-            <Query query={GET_INVOICE_GROUPS_DEFAULTS_QUERY} variables={{}}>
-             {({ loading, error, data: {financeInvoiceGroupDefaults: invoice_group_defaults}, refetch, fetchMore }) => {
-                // Loading
-                if (loading) return (
-                  <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}>
-                    <Dimmer active={true}
-                            loader={true}>
-                    </Dimmer>
-                  </ContentCard>
-                )
-                // Error
-                if (error) return (
-                  <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}>
-                    <p>{t('finance.invoice_groups_defaults.error_loading')}</p>
-                  </ContentCard>
-                )
-                
-                // Empty list
-                if (!invoice_group_defaults.edges.length) { return (
-                  <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}>
-                    <p>
-                      {t('finance.invoice_groups_defaults.empty_list')}
-                    </p>
-                   
-                  </ContentCard>
-                )} else {   
-                // Life's good! :)
-                return (
-                  <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}
-                               pageInfo={invoice_group_defaults.pageInfo}
-                               onLoadMore={() => {
-                                fetchMore({
-                                  variables: {
-                                    after: invoice_group_defaults.pageInfo.endCursor
-                                  },
-                                  updateQuery: (previousResult, { fetchMoreResult }) => {
-                                    const newEdges = fetchMoreResult.financeInvoiceGroups.edges
-                                    const pageInfo = fetchMoreResult.financeInvoiceGroups.pageInfo
+function FinanceInvoiceGroupsDefaults({ t, history }) {
+  const { loading, error, data, fetchMore } = useQuery(GET_INVOICE_GROUPS_DEFAULTS_QUERY, {
+    variables: { archived: false }
+  })
 
-                                    return newEdges.length
-                                      ? {
-                                          // Put the new invoice_groups_defaults at the end of the list and update `pageInfo`
-                                          // so we have the new `endCursor` and `hasNextPage` values
-                                          financeInvoiceGroups: {
-                                            __typename: previousResult.financeInvoiceGroups.__typename,
-                                            edges: [ ...previousResult.financeInvoiceGroups.edges, ...newEdges ],
-                                            pageInfo
-                                          }
-                                        }
-                                      : previousResult
-                                  }
+  const [updateDefaultGroup] = useMutation(UPDATE_INVOICE_GROUP_DEFAULT)
+
+  // Loading
+  if (loading) return (
+    <FinanceInvoiceGroupDefaultsBase>
+      <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}>
+        <Dimmer active={true}
+                loader={true}>
+        </Dimmer>
+      </ContentCard>
+    </FinanceInvoiceGroupDefaultsBase>
+  )
+  // Error
+  if (error) return (
+    <FinanceInvoiceGroupDefaultsBase>
+      <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}>
+        <p>{t('finance.invoice_groups_defaults.error_loading')}</p>
+      </ContentCard>
+    </FinanceInvoiceGroupDefaultsBase>
+  )
+
+  const financeInvoiceGroupDefaults = data.financeInvoiceGroupDefaults
+  
+  // Empty list
+  if (!financeInvoiceGroupDefaults.edges.length) { return (
+    <FinanceInvoiceGroupDefaultsBase>
+      <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}>
+        <p>
+          {t('finance.invoice_groups_defaults.empty_list')}
+        </p>
+      </ContentCard>
+    </FinanceInvoiceGroupDefaultsBase>
+  )} 
+
+  return (
+    <FinanceInvoiceGroupDefaultsBase>
+      <ContentCard cardTitle={t('finance.invoice_groups_defaults.title')}
+                  pageInfo={financeInvoiceGroupDefaults.pageInfo}
+                  onLoadMore={() => {
+                    fetchMore({
+                      variables: {
+                        after: financeInvoiceGroupDefaults.pageInfo.endCursor
+                      },
+                      updateQuery: (previousResult, { fetchMoreResult }) => {
+                        const newEdges = fetchMoreResult.financeInvoiceGroups.edges
+                        const pageInfo = fetchMoreResult.financeInvoiceGroups.pageInfo
+
+                        return newEdges.length
+                          ? {
+                              // Put the new invoice_groups_defaults at the end of the list and update `pageInfo`
+                              // so we have the new `endCursor` and `hasNextPage` values
+                              financeInvoiceGroups: {
+                                __typename: previousResult.financeInvoiceGroups.__typename,
+                                edges: [ ...previousResult.financeInvoiceGroups.edges, ...newEdges ],
+                                pageInfo
+                              }
+                            }
+                          : previousResult
+                      }
+                    })
+                  }} 
+        >
+        <Table>
+          <Table.Header>
+            <Table.Row key={v4()}>
+              <Table.ColHeader>{t('finance.invoice_groups_defaults.item_type')}</Table.ColHeader>
+              <Table.ColHeader>{t('finance.invoice_groups_defaults.invoice_group')}</Table.ColHeader>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+              {financeInvoiceGroupDefaults.edges.map(({ node }) => (
+                <Table.Row key={v4()}>
+                  <Table.Col key={v4()}>
+                    { fetch_default_type_name(t, node.itemType) }
+                  </Table.Col>
+                  <Table.Col>
+                    <Formik
+                      initialValues={{ 
+                        financeInvoiceGroup:node.financeInvoiceGroup.id
+                      }}
+                      validationSchema={INVOICE_GROUP_DEFAULT_SCHEMA}
+                      onSubmit={(values, { setSubmitting }) => {
+                          console.log('submit values:')
+                          console.log(values)
+
+                          updateDefaultGroup({ variables: {
+                            input: {
+                              id: node.id,
+                              financeInvoiceGroup: values.financeInvoiceGroup
+                            }
+                          }, refetchQueries: [
+                              // {query: GET_INVOICE_GROUPS_QUERY, variables: {"archived": false }}
+                          ]})
+                          .then(({ data }) => {
+                              console.log('got data', data)
+                              toast.success((t('finance.invoice_groups.toast_edit_success')), {
+                                  position: toast.POSITION.BOTTOM_RIGHT
                                 })
-                              }} 
-                    >
-                    <Table>
-                      <Table.Header>
-                        <Table.Row key={v4()}>
-                          <Table.ColHeader>{t('finance.invoice_groups_defaults.item_type')}</Table.ColHeader>
-                          <Table.ColHeader>{t('finance.invoice_groups_defaults.invoice_group')}</Table.ColHeader>
-                        </Table.Row>
-                      </Table.Header>
-                      <Table.Body>
-                          {invoice_group_defaults.edges.map(({ node }) => (
-                            <Table.Row key={v4()}>
-                              <Table.Col key={v4()}>
-                                { fetch_default_type_name(t, node.itemType) }
-                              </Table.Col>
-                              <Table.Col>
-                                <Query query={GET_INVOICE_GROUPS_QUERY} variables={{archived: false}}>
-                                  {({ loading, error, data, refetch, fetchMore }) => {
-                                    // Loading
-                                    if (loading) return ( "Loading...")
-                                    // Error
-                                    if (error) return ( "error loading" )
-
-                                    const inputData = data
-
-                                    return (
-                                      <Mutation mutation={UPDATE_INVOICE_GROUP_DEFAULT} key={v4()}>
-                                        {(updateDefault, { data }) => (
-                                          <Formik
-                                            initialValues={{ 
-                                              financeInvoiceGroup:node.financeInvoiceGroup.id
-                                            }}
-                                            validationSchema={INVOICE_GROUP_DEFAULT_SCHEMA}
-                                            onSubmit={(values, { setSubmitting }) => {
-                                                console.log('submit values:')
-                                                console.log(values)
-
-                                                updateDefault({ variables: {
-                                                  input: {
-                                                    id: node.id,
-                                                    financeInvoiceGroup: values.financeInvoiceGroup
-                                                  }
-                                                }, refetchQueries: [
-                                                    // {query: GET_INVOICE_GROUPS_QUERY, variables: {"archived": false }}
-                                                ]})
-                                                .then(({ data }) => {
-                                                    console.log('got data', data)
-                                                    toast.success((t('finance.invoice_groups.toast_edit_success')), {
-                                                        position: toast.POSITION.BOTTOM_RIGHT
-                                                      })
-                                                  }).catch((error) => {
-                                                    toast.error((t('general.toast_server_error')) +  error, {
-                                                        position: toast.POSITION.BOTTOM_RIGHT
-                                                      })
-                                                    console.log('there was an error sending the query', error)
-                                                    setSubmitting(false)
-                                                  })
-                                            }}
-                                            >
-                                            {({ isSubmitting, errors, values, setFieldTouched, setFieldValue, submitForm }) => (
-                                              <FinanceInvoiceGroupDefaultForm
-                                                inputData={inputData}
-                                                isSubmitting={isSubmitting}
-                                                errors={errors}
-                                                values={values}
-                                                setFieldTouched={setFieldTouched}
-                                                setFieldValue={setFieldValue}
-                                                submitForm={submitForm}
-                                              />
-                                            )}
-                                        </Formik>
-                                        )}
-                                      </Mutation>
-                                    )}}
-                                  </Query>
-                                </Table.Col>
-                            </Table.Row>
-                          ))}
-                      </Table.Body>
-                    </Table>
-                  </ContentCard>
-                )}}
-             }
-            </Query>
-          </Grid.Col>
-          <Grid.Col md={3}>
-            <h5>{t("general.menu")}</h5>
-            <FinanceMenu activeLink='invoices'/>
-          </Grid.Col>
-        </Grid.Row>
-      </Container>
-    </div>
-  </SiteWrapper>
-);
+                            }).catch((error) => {
+                              toast.error((t('general.toast_server_error')) +  error, {
+                                  position: toast.POSITION.BOTTOM_RIGHT
+                                })
+                              console.log('there was an error sending the query', error)
+                              setSubmitting(false)
+                            })
+                      }}
+                      >
+                      {({ isSubmitting, errors, values, setFieldTouched, setFieldValue, submitForm }) => (
+                        <FinanceInvoiceGroupDefaultForm
+                          inputData={data}
+                          isSubmitting={isSubmitting}
+                          errors={errors}
+                          values={values}
+                          setFieldTouched={setFieldTouched}
+                          setFieldValue={setFieldValue}
+                          submitForm={submitForm}
+                        />
+                      )}
+                    </Formik>
+                  </Table.Col>
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table>
+      </ContentCard>
+    </FinanceInvoiceGroupDefaultsBase>
+  )
+};
 
 export default withTranslation()(withRouter(FinanceInvoiceGroupsDefaults))
