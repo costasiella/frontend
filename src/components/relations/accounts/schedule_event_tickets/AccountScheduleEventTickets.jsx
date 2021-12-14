76ac98a1be36ad86213461f25b5f160e6af3426c
Edwin van de Ven <edwin@simplycode.nl>
2021-12-14 15:14:07 +0100

    Improve layout for AccountTickets

--- src/components/relations/accounts/schedule_event_tickets/AccountScheduleEventTickets.jsx
+++ src/components/relations/accounts/schedule_event_tickets/AccountScheduleEventTickets.jsx
@@ -67,6 +67,7 @@ function AccountScheduleEventTickets({t, history, match}) {
       <ContentCard 
         cardTitle={t('relations.account.event_tickets.title')}
         pageInfo={accountScheduleEventTickets.pageInfo}
+        hasCardBody={false}
         onLoadMore={() => {
           fetchMore({
             variables: {
@@ -91,7 +92,7 @@ function AccountScheduleEventTickets({t, history, match}) {
           })
         }} 
       >
-        <Table>
+        <Table cards>
           <Table.Header>
             <Table.Row key={v4()}>
               <Table.ColHeader>{t('general.ticket')}</Table.ColHeader>
