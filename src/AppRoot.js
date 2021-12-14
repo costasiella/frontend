9dfedc80924e3ad6d5cdf1edce2cae05ec1e3a62
Edwin van de Ven <edwin@simplycode.nl>
2021-12-14 15:05:34 +0100

    Add route to account classpasses component

--- src/AppRoot.js
+++ src/AppRoot.js
@@ -158,6 +158,7 @@ import RelationsAccountClasses from './components/relations/accounts/classes/Acc
 import AccountClasspasses from './components/relations/accounts/classpasses/AccountClasspasses'
 import AccountClasspassAdd from './components/relations/accounts/classpasses/AccountClasspassAdd'
 import AccountClasspassEdit from './components/relations/accounts/classpasses/AccountClasspassEdit'
+import AccountClasspassClasses from './components/relations/accounts/classpass_classes/AccountClasspassClasses'
 import AccountDocuments from './components/relations/accounts/documents/AccountDocuments'
 import AccountDocumentAdd from './components/relations/accounts/documents/AccountDocumentAdd'
 import AccountDocumentEdit from './components/relations/accounts/documents/AccountDocumentEdit'
@@ -548,6 +549,7 @@ function AppRoot({ t }) {
             <PrivateRoute exact path="/relations/accounts/:account_id/classpasses" component={AccountClasspasses} />
             <PrivateRoute exact path="/relations/accounts/:account_id/classpasses/add" component={AccountClasspassAdd} />
             <PrivateRoute exact path="/relations/accounts/:account_id/classpasses/edit/:id" component={AccountClasspassEdit} />
+            <PrivateRoute exact path="/relations/accounts/:account_id/classpasses/classes/:id" component={AccountClasspassClasses} />
             <PrivateRoute exact path="/relations/accounts/:account_id/documents" component={AccountDocuments} />
             <PrivateRoute exact path="/relations/accounts/:account_id/documents/add" component={AccountDocumentAdd} />
             <PrivateRoute exact path="/relations/accounts/:account_id/documents/edit/:id" component={AccountDocumentEdit} />
