0e7a1e45edaebd61ba7e27dad014c7b641ef40a1
Edwin van de Ven <edwin@simplycode.nl>
2021-12-14 15:16:11 +0100

    Remove custom back button in RelationsAccountsBack

--- src/components/relations/accounts/RelationsAccountsBack.jsx
+++ src/components/relations/accounts/RelationsAccountsBack.jsx
@@ -8,12 +8,11 @@ import {
   Icon,
 } from "tabler-react";
 
+import ButtonBack from '../../ui/ButtonBack'
+
 const RelationsAccountsBack = ({ t }) => (
   <div className="page-options d-flex">
-    <Link to="/relations/accounts" 
-          className='btn btn-outline-secondary btn-sm'>
-        <Icon prefix="fe" name="arrow-left" /> {t('general.back_to')} {t('relations.accounts.title')}
-    </Link>
+    <ButtonBack returnUrl="/relations/accounts" />
   </div>
 )
 
