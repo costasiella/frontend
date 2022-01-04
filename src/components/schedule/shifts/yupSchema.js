f1a7f3a95614aad1dbb40edbd7cdc7476352aebe
Edwin van de Ven <edwin@simplycode.nl>
2022-01-04 16:03:07 +0100

    Add yupSchema for shifts

---
+++ src/components/schedule/shifts/yupSchema.js
@@ -0,0 +1,19 @@
+import { t } from 'i18next'
+import * as Yup from 'yup'
+
+export const SHIFT_SCHEMA = Yup.object().shape({
+    frequencyType: Yup.string()
+      .required(t('yup.field_required')),
+    organizationLocationRoom: Yup.string()
+      .required(t('yup.field_required')),
+    organizationShift: Yup.string()
+      .required(t('yup.field_required')),
+    dateStart: Yup.date()
+      .required(t('yup.field_required')),
+    timeStart: Yup.date()
+      .typeError(t('yup.time_required'))
+      .required(t('yup.field_required')),
+    timeEnd: Yup.date()
+      .typeError(t('yup.time_required'))
+      .required(t('yup.field_required')),
+  })
