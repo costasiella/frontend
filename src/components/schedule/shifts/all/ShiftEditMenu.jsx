a672aaee6ab1fb218872264e97ed692b79f4efcc
Edwin van de Ven <edwin@simplycode.nl>
2022-01-05 12:41:09 +0100

    Add ShiftEditMenu component

---
+++ src/components/schedule/shifts/all/ShiftEditMenu.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { v4 } from "uuid"
+import { withTranslation } from 'react-i18next'
+
+
+import {
+  List
+} from "tabler-react";
+import HasPermissionWrapper from "../HasPermissionWrapper"
+
+
+const ShiftEditMenu = ({ t, activeLink, classId }) => (
+  <List.Group transparent={true}>
+    <List.GroupItem
+      key={v4()}
+      className="d-flex align-items-center"
+      to={"#/schedule/classes/all/edit/" + classId}
+      icon="edit-3"
+      active={(activeLink==="edit")}
+      >
+      {t('general.edit')}
+    </List.GroupItem>
+    <HasPermissionWrapper permission="view"
+                          resource="scheduleitemaccount">
+      <List.GroupItem
+        key={v4()}
+        className="d-flex align-items-center"
+        to={"#/schedule/classes/all/teachers/" + classId}
+        icon="users"
+        active={activeLink==="teachers"}
+        >
+        {t('general.teachers')}
+      </List.GroupItem>
+    </HasPermissionWrapper>
+  </List.Group>
+);
+
+export default withTranslation()(ShiftEditMenu)
