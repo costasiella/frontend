507ec733767ec395763518af5d83ef5446c3dfe7
Edwin van de Ven <edwin@simplycode.nl>
2022-01-05 17:05:44 +0100

    Improve display of shift subtitle

--- src/components/schedule/shifts/all/tools.js
+++ src/components/schedule/shifts/all/tools.js
@@ -1,5 +1,5 @@
 import moment from 'moment'
 
-export function shift_edit_all_subtitle({t, location, locationRoom, classtype, starttime}) {
-  return t('general.shift') + ': ' + location + ' (' + locationRoom + ') - ' + classtype + ' @ ' + moment(starttime).format('LT')
+export function shift_edit_all_subtitle({t, location, locationRoom, shift, starttime}) {
+  return t('general.shift') + ': ' + location + ' (' + locationRoom + ') - ' + shift + ' @ ' + moment(starttime).format('LT')
 }
