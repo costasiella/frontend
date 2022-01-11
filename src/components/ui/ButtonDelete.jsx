c0a340b051d39c8230d4c6f05a81638e28f63f96
Edwin van de Ven <edwin@simplycode.nl>
2022-01-11 13:30:00 +0100

    Add ButtonDelete component

---
+++ src/components/ui/ButtonDelete.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import confirm_delete from "../../tools/confirm_delete"
+
+import {
+  Icon
+} from "tabler-react"
+
+
+function ButtonDelete({t, match, history, msgConfirm, msgDescription, msgSuccess, deleteFunction, deleteFunctionVariables}) {
+  return (
+    <button className="icon btn btn-link btn-sm"
+      title={t('general.delete')} 
+      onClick={() => {
+        confirm_delete({
+          t: t,
+          msgConfirm: <p>{msgConfirm}</p>,
+          msgDescription: msgDescription,
+          msgSuccess: msgSuccess,
+          deleteFunction: deleteFunction,
+          deleteFunctionVariables: deleteFunctionVariables
+        })
+    }}>
+      <span className="text-red">
+        <Icon prefix="fe" name="trash-2" />
+      </span>
+    </button> 
+  )
+}
+
+
+export default withTranslation()(withRouter(ButtonDelete))
