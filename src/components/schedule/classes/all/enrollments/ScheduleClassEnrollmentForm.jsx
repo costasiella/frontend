057f43ffbd0b5eabffd78ef15ec76d23bfa62c7a
Edwin van de Ven <edwin@simplycode.nl>
2022-01-12 17:11:01 +0100

    Add ScheculeClassEnrollmentForm

---
+++ src/components/schedule/classes/all/enrollments/ScheduleClassEnrollmentForm.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Form as FoForm, Field, ErrorMessage } from 'formik'
+
+import {
+  Button,
+  Card,
+  Grid,
+  Form,
+} from "tabler-react"
+
+import CSDatePicker from "../../../../ui/CSDatePicker"
+
+
+const ScheduleClassEnrollmentForm = ({ t, history, match, isSubmitting, errors, values, returnUrl, setFieldTouched, setFieldValue }) => (
+  <FoForm>
+      <Card.Body>
+        <Grid.Row>
+          <Grid.Col>
+            <Form.Group label={t('general.date_start')}>
+              <CSDatePicker 
+                selected={values.dateStart}
+                onChange={(date) => setFieldValue("dateStart", date)}
+                onBlur={() => setFieldTouched("dateStart", true)}
+              />
+              <ErrorMessage name="dateStart" component="span" className="invalid-feedback" />
+            </Form.Group>
+          </Grid.Col>
+          <Grid.Col>
+            <Form.Group label={t('general.date_end')}>
+              <CSDatePicker 
+                selected={values.dateEnd}
+                onChange={(date) => setFieldValue("dateEnd", date)}
+                onBlur={() => setFieldTouched("dateEnd", true)}
+              />
+              <ErrorMessage name="dateEnd" component="span" className="invalid-feedback" />
+            </Form.Group>
+          </Grid.Col>
+        </Grid.Row>
+      </Card.Body>
+      <Card.Footer>
+          <Button 
+            color="primary"
+            className="pull-right" 
+            type="submit" 
+            disabled={isSubmitting}
+          >
+            {t('general.submit')}
+          </Button>
+          <Button color="link" onClick={() => history.push(returnUrl)} role="button">
+              {t('general.cancel')}
+          </Button>
+      </Card.Footer>
+  </FoForm>
+);
+
+export default withTranslation()(withRouter(ScheduleClassEnrollmentForm))
