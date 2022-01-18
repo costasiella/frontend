e61abb62fe17fafac40d13c509d3bf9e6c1024ea
Edwin van de Ven <edwin@simplycode.nl>
2022-01-18 12:57:29 +0100

    Clean up imports

--- src/components/schedule/classes/all/enrollments/ScheduleClassEnrollmentOptions.jsx
+++ src/components/schedule/classes/all/enrollments/ScheduleClassEnrollmentOptions.jsx
@@ -2,18 +2,10 @@ import React from 'react'
 import { useMutation, useQuery } from "@apollo/client";
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-import { Formik } from 'formik'
-import { toast } from 'react-toastify'
 import { Dimmer, Grid } from 'tabler-react';
-
-
 
 import ScheduleClassEnrollSubscriptions from "./ScheduleClassEnrollSubscriptions"
 import { GET_SCHEDULE_ITEM_ENROLLMENT_OPTIONS_QUERY, CREATE_SCHEDULE_ITEM_ENROLLMENT } from './queries'
-import { SCHEDULE_CLASS_ENROLLMENT_SCHEMA } from './yupSchema'
-import ScheduleClassEnrollmentForm from './ScheduleClassEnrollmentForm'
-import { dateToLocalISO } from '../../../../../tools/date_tools'
-
 import ClassEditBase from "../ClassEditBase"
 import ScheduleClassEnrollmentBack from "./ScheduleClassEnrollmentBack"
 
