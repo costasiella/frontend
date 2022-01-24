08b7e686cd30eeb80745a3dbecf5d1bdb9444c27
Edwin van de Ven <edwin@simplycode.nl>
2022-01-24 12:23:51 +0100

    delete subscription price button

--- src/components/organization/subscriptions/prices/OrganizationSubscriptionsPrices.jsx
+++ src/components/organization/subscriptions/prices/OrganizationSubscriptionsPrices.jsx
@@ -3,24 +3,14 @@ import { useQuery, useMutation } from "@apollo/client"
 import { v4 } from "uuid"
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-import { Link } from 'react-router-dom'
-import { toast } from 'react-toastify'
-import { confirmAlert } from 'react-confirm-alert'
 import moment from 'moment'
-
 
 import {
   Card,
   Dimmer,
-  Page,
-  Grid,
-  Icon,
   Button,
-  Container,
   Table
 } from "tabler-react";
-import SiteWrapper from "../../../SiteWrapper"
-import HasPermissionWrapper from "../../../HasPermissionWrapper"
 
 import AppSettingsContext from '../../../context/AppSettingsContext'
 import ContentCard from "../../../general/ContentCard"
@@ -29,63 +19,10 @@ import OrganizationSubscriptionsPricesBase from './OrganizationSubscriptionsPric
 import { GET_SUBSCRIPTION_PRICES_QUERY, DELETE_SUBSCRIPTION_PRICE } from "./queries"
 import { GET_SUBSCRIPTIONS_QUERY } from "../queries"
 
-import AlertInfo from "../../../ui/AlertInfo"
 import ButtonDelete from '../../../ui/ButtonDelete'
 
 
-const confirmDelete = (t, match, deleteSubscriptionPrice, node) => {
-  console.log("clicked delete")
-  let id = node.id
-
-  confirmAlert({
-    customUI: ({ onClose }) => {
-      return (
-        <div className='custom-ui'>
-          <h1>{t('general.confirm_delete')}</h1>
-          <p>{t('organization.subscription_prices.delete_confirm_msg')}</p>
-          <p>
-            {node.priceDisplay} {node.financeTaxRate.name} <br />
-            <span className="text-muted">
-              {node.dateStart} {(node.dateEnd) ? ' - ' + node.dateEnd : ""}
-            </span>
-          </p>
-          <button className="btn btn-link pull-right" onClick={onClose}>{t('general.confirm_delete_no')}</button>
-          <button
-            className="btn btn-danger"
-            onClick={() => {
-            deleteSubscriptionPrice({ variables: {
-                input: {
-                id
-                }
-              }, refetchQueries: [
-                  {query: GET_SUBSCRIPTION_PRICES_QUERY, variables: { organizationSubscription: match.params.subscription_id }},
-                  {query: GET_SUBSCRIPTIONS_QUERY, variables: {archived: false}},
-              ]}).then(({ data }) => {
-                console.log('got data', data);
-                toast.success(
-                  t('organization.subscription_prices.deleted'), {
-                    position: toast.POSITION.BOTTOM_RIGHT
-                  })
-              }).catch((error) => {
-                toast.error((t('general.toast_server_error')) +  error, {
-                    position: toast.POSITION.BOTTOM_RIGHT
-                  })
-                console.log('there was an error sending the query', error);
-              })
-              onClose()
-            }}
-          >
-            <Icon name="trash-2" /> {t('general.confirm_delete_yes')}
-          </button>
-          
-        </div>
-      );
-    }
-  })
-}
-
-
-function OrganizationSubscriptionsPrices ({ t, history, match, archived=false }) {
+function OrganizationSubscriptionsPrices ({ t, history, match }) {
   const appSettings = useContext(AppSettingsContext)
   const dateFormat = appSettings.dateFormat
   const subscriptionId = match.params.subscription_id
@@ -187,11 +124,30 @@ function OrganizationSubscriptionsPrices ({ t, history, match, archived=false })
                     {(node.archived) ? 
                       <span className='text-muted'>{t('general.unarchive_to_edit')}</span> :
                       <Button className='btn-sm' 
-                              onClick={() => history.push("/organization/subscriptions/prices/edit/" + match.params.subscription_id + '/' + node.id)}
+                              onClick={() => history.push("/organization/subscriptions/prices/edit/" + subscriptionId + '/' + node.id)}
                               color="secondary">
                         {t('general.edit')}
                       </Button>
                     }
+                    <ButtonDelete 
+                      msgConfirm={t('organization.subscription_prices.delete_confirm_msg')}
+                      msgDescription={<p>
+                        {node.priceDisplay} {node.financeTaxRate.name} <br />
+                        <span className="text-muted">
+                          {node.dateStart} {(node.dateEnd) ? ' - ' + node.dateEnd : ""}
+                        </span>
+                      </p>}
+                      msgSuccess={t('organization.subscription_prices.deleted')}
+                      deleteFunction={deleteSubscriptionPrice}
+                      deleteFunctionVariables={{ variables: {
+                        input: {
+                          id: node.id
+                        }
+                      }, refetchQueries: [
+                          {query: GET_SUBSCRIPTION_PRICES_QUERY, variables: { organizationSubscription: subscriptionId }},
+                          {query: GET_SUBSCRIPTIONS_QUERY, variables: {archived: false}},
+                      ]}}
+                    />
                   </Table.Col>
                   {/* <Mutation mutation={DELETE_SUBSCRIPTION_PRICE} key={v4()}>
                       {(deleteSubscriptionPrice, { data }) => (
