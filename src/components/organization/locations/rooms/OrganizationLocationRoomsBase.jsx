53af17bcc8e88d5d261b3bf04fc6871329914de6
Edwin van de Ven <edwin@simplycode.nl>
2022-02-12 14:21:52 +0100

    Update location rooms

--- src/components/organization/locations/rooms/OrganizationLocationRoomsBase.jsx
+++ src/components/organization/locations/rooms/OrganizationLocationRoomsBase.jsx
@@ -12,11 +12,11 @@ import {
 } from "tabler-react";
 import SiteWrapper from "../../../SiteWrapper"
 import HasPermissionWrapper from "../../../HasPermissionWrapper"
+import ButtonAdd from '../../../ui/ButtonAdd';
+import ButtonBack from '../../../ui/ButtonBack';
 
 
-
-
-function OrganizationLocationRoomsBase({t, history, match, children, showBack=false}) {
+function OrganizationLocationRoomsBase({t, history, match, children, showAdd=false, returnUrl="/organization/locations"}) {
   const locationId = match.params.location_id
 
   return (
@@ -25,34 +25,21 @@ function OrganizationLocationRoomsBase({t, history, match, children, showBack=fa
         <Container>
           <Page.Header title={t("organization.title")}>
             <div className="page-options d-flex">
-              <Link to="/organization/locations" 
-                    className='btn btn-outline-secondary btn-sm'>
-                  <Icon prefix="fe" name="arrow-left" /> {t('general.back_to')} {t('organization.locations.title')}
-              </Link>
+              <ButtonBack returnUrl={returnUrl} />
+              {(showAdd) ? 
+                <HasPermissionWrapper permission="add"
+                                      resource="organizationlocationroom">
+                  <ButtonAdd addUrl={`/organization/locations/rooms/add/${match.params.location_id}`}
+                    className="ml-2" />
+                </HasPermissionWrapper>
+                : ""
+              }
             </div>
           </Page.Header>
           <Grid.Row>
-            <Grid.Col md={9}>
+            <Grid.Col md={12}>
               {children}
             </Grid.Col>
-            <Grid.Col md={3}>
-              {(showBack) ?
-                <Link to={`/organization/locations/rooms/${locationId}`}>
-                  <Button color="primary btn-block mb-6">
-                    <Icon prefix="fe" name="chevrons-left" /> {t('general.back')}
-                  </Button>
-                </Link>
-              :
-                <HasPermissionWrapper permission="add"
-                                      resource="organizationlocationroom">
-                  <Link to={`/organization/locations/rooms/add/${match.params.location_id}`}>
-                    <Button color="primary btn-block mb-6">
-                      <Icon prefix="fe" name="plus-circle" /> {t('organization.location_rooms.add')}
-                    </Button>
-                  </Link>
-                </HasPermissionWrapper>
-              }
-          </Grid.Col>
         </Grid.Row>
       </Container>
     </div>
