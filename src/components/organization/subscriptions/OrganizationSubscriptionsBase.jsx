f47857d5eb7e022f5c25d23b0cecd6c0f81c1664
Edwin van de Ven <edwin@simplycode.nl>
2022-02-12 18:54:11 +0100

    Update organization subscriptions

--- src/components/organization/subscriptions/OrganizationSubscriptionsBase.jsx
+++ src/components/organization/subscriptions/OrganizationSubscriptionsBase.jsx
@@ -1,5 +1,3 @@
-// @flow
-
 import React from 'react'
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
@@ -14,6 +12,8 @@ import {
 } from "tabler-react";
 import SiteWrapper from "../../SiteWrapper"
 import HasPermissionWrapper from "../../HasPermissionWrapper"
+import ButtonAdd from '../../ui/ButtonAdd'
+import ButtonBack from '../../ui/ButtonBack'
 
 
 
@@ -26,33 +26,25 @@ function OrganizationSubscriptionsBase({t, children, showBack=false}) {
         <Container>
           <Page.Header title={t("organization.title")}>
             <div className="page-options d-flex">
-              <Link to="/organization/subscriptions/groups" 
-                    className='btn btn-outline-secondary btn-sm'>
-                  <Icon prefix="fe" name="folder" /> {t('general.groups')}
-              </Link>
+              {(showBack) ? 
+                <ButtonBack returnUrl="/organization/subscriptions" />
+              :
+                <React.Fragment>
+                  <Link to="/organization/subscriptions/groups" 
+                        className='btn btn-secondary ml-2 mr-2'>
+                      <Icon prefix="fe" name="folder" /> {t('general.groups')}
+                  </Link>
+                  <HasPermissionWrapper permission="add"
+                                      resource="organizationsubscription">
+                    <ButtonAdd addUrl="/organization/subscriptions/add" />
+                  </HasPermissionWrapper>
+                </React.Fragment>
+              }
             </div>
           </Page.Header>
           <Grid.Row>
-            <Grid.Col md={9}>
+            <Grid.Col md={12}>
               {children}
-            </Grid.Col>
-            <Grid.Col md={3}>
-              {(showBack) ? 
-                <Link to="/organization/subscriptions/">
-                  <Button color="primary btn-block mb-6">
-                    <Icon prefix="fe" name="chevrons-left" /> {t('general.back')}
-                  </Button>
-                </Link>
-              :
-                <HasPermissionWrapper permission="add"
-                                      resource="organizationsubscription">
-                  <Link to="/organization/subscriptions/add">
-                    <Button color="primary btn-block mb-6">
-                      <Icon prefix="fe" name="plus-circle" /> {t('organization.subscriptions.add')}
-                    </Button>
-                  </Link>
-                </HasPermissionWrapper>
-              }
             </Grid.Col>
           </Grid.Row>
         </Container>
