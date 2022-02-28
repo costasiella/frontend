09b2329edb36e7655ab810eebbe743a8a3f5d18a
Edwin van de Ven <edwin@simplycode.nl>
2022-02-28 12:09:32 +0100

    Improve customers list layout

--- src/components/schedule/events/tickets/customers/ScheduleEventTicketEditCustomers.jsx
+++ src/components/schedule/events/tickets/customers/ScheduleEventTicketEditCustomers.jsx
@@ -77,105 +77,103 @@ function ScheduleEventTicketEditCustomers({ t, history, match }) {
       returnUrl={returnUrl}
       pageHeaderOptions={pageHeaderOptions}
     >
-      <Card.Body>
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.ColHeader>{t('general.name')}</Table.ColHeader>
-              <Table.ColHeader>{t('general.invoice')}</Table.ColHeader>
-              <Table.ColHeader>{t('schedule.events.tickets.info_mail_sent')}</Table.ColHeader> 
-              <Table.ColHeader></Table.ColHeader> 
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {data.accountScheduleEventTickets.edges.map(({ node }) => (
-              <Table.Row key={v4()}>
-                <Table.Col>
-                  {node.account.fullName} <br />
-                  {(node.cancelled) ? <Badge color="warning">{t("general.cancelled")}</Badge> : ""}
-                </Table.Col>  
-                <Table.Col>
-                  { node.invoiceItems.edges && <Link to={`/finance/invoices/edit/${node.invoiceItems.edges[0].node.financeInvoice.id}`}>
-                      {node.invoiceItems.edges[0].node.financeInvoice.invoiceNumber } <br />
-                    </Link>
-                  }
-                  <small className="text-muted">
-                   {node.invoiceItems.edges && node.invoiceItems.edges[0].node.financeInvoice.summary } <br />
-                  </small>
-                </Table.Col>
-                <Table.Col>
-                  <BadgeBoolean value={node.infoMailSent} /> <br />
-                  {/* TODO: resend link here */}
-                </Table.Col>
-                <Table.Col>
-                  {(node.cancelled) ?
-                    <Button 
-                      className="pull-right"
-                      color="warning"
-                      onClick={() =>
-                        updateAccountScheduleEventTicket({ variables: {
-                          input: {
-                            id: node.id,
-                            cancelled: false
-                          }
-                        }, refetchQueries: [
-                            {query: GET_ACCOUNT_SCHEDULE_EVENT_TICKETS_QUERY, variables: {
-                              scheduleEventTicket: id
-                            }},
-                        ]})
-                        .then(({ data }) => {
-                            console.log('got data', data);
-                            toast.success((t('schedule.events.tickets.customers.uncancelled')), {
-                                position: toast.POSITION.BOTTOM_RIGHT
-                              })
-                          }).catch((error) => {
-                            toast.error((t('general.toast_server_error')) +  error, {
-                                position: toast.POSITION.BOTTOM_RIGHT
-                              })
-                            console.log('there was an error sending the query', error)
-                          }
-                        )
+      <Table cards>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColHeader>{t('general.name')}</Table.ColHeader>
+            <Table.ColHeader>{t('general.invoice')}</Table.ColHeader>
+            <Table.ColHeader>{t('schedule.events.tickets.info_mail_sent')}</Table.ColHeader> 
+            <Table.ColHeader></Table.ColHeader> 
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {data.accountScheduleEventTickets.edges.map(({ node }) => (
+            <Table.Row key={v4()}>
+              <Table.Col>
+                {node.account.fullName} <br />
+                {(node.cancelled) ? <Badge color="warning">{t("general.cancelled")}</Badge> : ""}
+              </Table.Col>  
+              <Table.Col>
+                { node.invoiceItems.edges && <Link to={`/finance/invoices/edit/${node.invoiceItems.edges[0].node.financeInvoice.id}`}>
+                    {node.invoiceItems.edges[0].node.financeInvoice.invoiceNumber } <br />
+                  </Link>
+                }
+                <small className="text-muted">
+                  {node.invoiceItems.edges && node.invoiceItems.edges[0].node.financeInvoice.summary.trunc(40) } <br />
+                </small>
+              </Table.Col>
+              <Table.Col>
+                <BadgeBoolean value={node.infoMailSent} /> <br />
+                {/* TODO: resend link here */}
+              </Table.Col>
+              <Table.Col>
+                {(node.cancelled) ?
+                  <Button 
+                    className="pull-right"
+                    color="warning"
+                    onClick={() =>
+                      updateAccountScheduleEventTicket({ variables: {
+                        input: {
+                          id: node.id,
+                          cancelled: false
+                        }
+                      }, refetchQueries: [
+                          {query: GET_ACCOUNT_SCHEDULE_EVENT_TICKETS_QUERY, variables: {
+                            scheduleEventTicket: id
+                          }},
+                      ]})
+                      .then(({ data }) => {
+                          console.log('got data', data);
+                          toast.success((t('schedule.events.tickets.customers.uncancelled')), {
+                              position: toast.POSITION.BOTTOM_RIGHT
+                            })
+                        }).catch((error) => {
+                          toast.error((t('general.toast_server_error')) +  error, {
+                              position: toast.POSITION.BOTTOM_RIGHT
+                            })
+                          console.log('there was an error sending the query', error)
+                        }
+                      )
+                    }
+                  >
+                    {t("general.uncancel")}
+                  </Button>
+                :
+                  <Button 
+                    className="pull-right"
+                    color="warning"
+                    onClick={() =>
+                      updateAccountScheduleEventTicket({ variables: {
+                        input: {
+                          id: node.id,
+                          cancelled: true
+                        }
+                      }, refetchQueries: [
+                          {query: GET_ACCOUNT_SCHEDULE_EVENT_TICKETS_QUERY, variables: {
+                            scheduleEventTicket: id
+                          }},
+                      ]})
+                      .then(({ data }) => {
+                          console.log('got data', data);
+                          toast.success((t('schedule.events.tickets.customers.cancelled')), {
+                              position: toast.POSITION.BOTTOM_RIGHT
+                            })
+                        }).catch((error) => {
+                          toast.error((t('general.toast_server_error')) +  error, {
+                              position: toast.POSITION.BOTTOM_RIGHT
+                            })
+                          console.log('there was an error sending the query', error)
+                        })
                       }
                     >
-                      {t("general.uncancel")}
+                      {t("general.cancel")}
                     </Button>
-                  :
-                    <Button 
-                      className="pull-right"
-                      color="warning"
-                      onClick={() =>
-                        updateAccountScheduleEventTicket({ variables: {
-                          input: {
-                            id: node.id,
-                            cancelled: true
-                          }
-                        }, refetchQueries: [
-                            {query: GET_ACCOUNT_SCHEDULE_EVENT_TICKETS_QUERY, variables: {
-                              scheduleEventTicket: id
-                            }},
-                        ]})
-                        .then(({ data }) => {
-                            console.log('got data', data);
-                            toast.success((t('schedule.events.tickets.customers.cancelled')), {
-                                position: toast.POSITION.BOTTOM_RIGHT
-                              })
-                          }).catch((error) => {
-                            toast.error((t('general.toast_server_error')) +  error, {
-                                position: toast.POSITION.BOTTOM_RIGHT
-                              })
-                            console.log('there was an error sending the query', error)
-                          })
-                        }
-                      >
-                        {t("general.cancel")}
-                      </Button>
-                  }
-                </Table.Col>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      </Card.Body>
+                }
+              </Table.Col>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </ScheduleEventTicketEditBase>
   )
 }
