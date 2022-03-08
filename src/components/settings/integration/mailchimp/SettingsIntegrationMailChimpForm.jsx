98c32ca64f56ca2569d7614ebbeffa1adba318c3
Edwin van de Ven <edwin@simplycode.nl>
2022-03-08 09:27:33 +0100

    Add MailChimpForm component

---
+++ src/components/settings/integration/mailchimp/SettingsIntegrationMailChimpForm.jsx
@@ -0,0 +1,53 @@
+// @flow
+
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Form as FoForm, Field, ErrorMessage } from 'formik'
+
+
+import {
+  Button,
+  Card,
+  Form,
+} from "tabler-react"
+
+
+const SettingsIntegrationMollieForm = ({ t, history, isSubmitting, errors, values, returnUrl }) => (
+  <FoForm>
+      <Card.Body>
+          <Form.Group label={t('settings.integration.mailchimp.user')}>
+            <Field type="text" 
+              name="mailchimp_user" 
+              className={(errors.mailchimp_user) ? "form-control is-invalid" : "form-control"} 
+              autoComplete="off" />
+            <ErrorMessage name="mailchimp_user" component="span" className="invalid-feedback" />
+          </Form.Group>
+          <Form.Group label={t('settings.integration.mailchimp.api_key')}>
+            <Field type="text" 
+              name="mailchimp_api_key" 
+              className={(errors.mailchimp_api_key) ? "form-control is-invalid" : "form-control"} 
+              autoComplete="off" />
+            <ErrorMessage name="mailchimp_api_key" component="span" className="invalid-feedback" />
+          </Form.Group>
+      </Card.Body>
+      <Card.Footer>
+          <Button 
+            color="primary"
+            type="submit" 
+            disabled={isSubmitting}
+          >
+            {t('general.submit')}
+          </Button>
+          {/* <Link to={returnUrl}>
+            <Button 
+              type="button" 
+              color="link">
+                {t('general.cancel')}
+            </Button>
+          </Link> */}
+      </Card.Footer>
+  </FoForm>
+)
+
+export default withTranslation()(withRouter(SettingsIntegrationMollieForm))
