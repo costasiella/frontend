a4981c4994e15164ae433679107edaf8f2598c60
Edwin van de Ven <edwin@simplycode.nl>
2022-03-13 13:46:03 +0100

    Add ShopaccountMailingListsBase component

---
+++ src/components/shop/account/mailing_lists/queries.js
@@ -0,0 +1,40 @@
+import { gql } from "@apollo/client"
+
+
+export const QUERY_SYSTEM_MAILCHIMP_LISTS = gql`
+  query SystemMailchimpLists($before: String, $after: String) {
+    systemMailchimpLists(first: 100, before: $before, after: $after) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          name
+          description
+          frequency
+        }
+      }
+    }
+    user {
+      id
+      accountId
+      firstName
+      lastName
+      fullName
+      email
+      gender
+      dateOfBirth
+      address
+      postcode
+      city
+      country
+      phone
+      mobile
+      emergency
+    }
+  }
+`
