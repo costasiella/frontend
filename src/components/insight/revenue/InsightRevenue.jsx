274349fbc0333459f54717f86407d780ee8cd490
Edwin van de Ven <edwin@simplycode.nl>
2022-04-06 13:49:05 +0200

    Optimize insight revenue other

--- src/components/insight/revenue/InsightRevenue.jsx
+++ src/components/insight/revenue/InsightRevenue.jsx
@@ -4,21 +4,7 @@ import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
 
 import CSLS from "../../../tools/cs_local_storage"
-import { 
-  GET_REVENUE_QUERY, 
-  GET_REVENUE_TOTAL_CLASSPASSES_QUERY,
-  GET_REVENUE_SUBTOTAL_CLASSPASSES_QUERY,
-  GET_REVENUE_TAX_CLASSPASSES_QUERY,
-  GET_REVENUE_TOTAL_EVENT_TICKETS_QUERY,
-  GET_REVENUE_SUBTOTAL_EVENT_TICKETS_QUERY,
-  GET_REVENUE_TAX_EVENT_TICKETS_QUERY,
-  GET_REVENUE_TOTAL_OTHER_QUERY,
-  GET_REVENUE_SUBTOTAL_OTHER_QUERY,
-  GET_REVENUE_TAX_OTHER_QUERY,
-  GET_REVENUE_TOTAL_SUBSCRIPTIONS_QUERY, 
-  GET_REVENUE_SUBTOTAL_SUBSCRIPTIONS_QUERY, 
-  GET_REVENUE_TAX_SUBSCRIPTIONS_QUERY
-} from './queries'
+import { GET_REVENUE_QUERY } from './queries'
 import InsightRevenueBase from './InsightRevenueBase'
 import InsightRevenueDisplay from './InsightRevenueDisplay'
 
@@ -34,142 +20,9 @@ function InsightRevenue ({ t, history }) {
     variables: { year: year }
   })
 
-  // // Classpasses
-  // const { 
-  //   loading: loadingTotalPasses, 
-  //   error: errorTotalPasses, 
-  //   data: dataTotalPasses,
-  //   refetch: refetchTotalPasses
-  //  } = useQuery(GET_REVENUE_TOTAL_CLASSPASSES_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingSubtotalPasses, 
-  //   error: errorSubtotalPasses, 
-  //   data: dataSubtotalPasses,
-  //   refetch: refetchSubtotalPasses
-  //  } = useQuery(GET_REVENUE_SUBTOTAL_CLASSPASSES_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingTaxPasses, 
-  //   error: errorTaxPasses, 
-  //   data: dataTaxPasses,
-  //   refetch: refetchTaxPasses
-  //  } = useQuery(GET_REVENUE_TAX_CLASSPASSES_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // // Subscriptions
-  // const { 
-  //   loading: loadingTotalSubs, 
-  //   error: errorTotalSubs, 
-  //   data: dataTotalSubs,
-  //   refetch: refetchTotalSubs
-  //  } = useQuery(GET_REVENUE_TOTAL_SUBSCRIPTIONS_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingSubtotalSubs, 
-  //   error: errorSubtotalSubs, 
-  //   data: dataSubtotalSubs,
-  //   refetch: refetchSubtotalSubs
-  //  } = useQuery(GET_REVENUE_SUBTOTAL_SUBSCRIPTIONS_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingTaxSubs, 
-  //   error: errorTaxSubs, 
-  //   data: dataTaxSubs,
-  //   refetch: refetchTaxSubs
-  //  } = useQuery(GET_REVENUE_TAX_SUBSCRIPTIONS_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // // Event tickets
-  // const { 
-  //   loading: loadingTotalTickets, 
-  //   error: errorTotalTickets, 
-  //   data: dataTotalTickets,
-  //   refetch: refetchTotalTickets
-  //  } = useQuery(GET_REVENUE_TOTAL_EVENT_TICKETS_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingSubtotalTickets, 
-  //   error: errorSubtotalTickets, 
-  //   data: dataSubtotalTickets,
-  //   refetch: refetchSubtotalTickets
-  //  } = useQuery(GET_REVENUE_SUBTOTAL_EVENT_TICKETS_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingTaxTickets, 
-  //   error: errorTaxTickets, 
-  //   data: dataTaxTickets,
-  //   refetch: refetchTaxTickets
-  //  } = useQuery(GET_REVENUE_TAX_EVENT_TICKETS_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // // other
-  // const { 
-  //   loading: loadingTotalOther, 
-  //   error: errorTotalOther, 
-  //   data: dataTotalOther,
-  //   refetch: refetchTotalOther
-  //  } = useQuery(GET_REVENUE_TOTAL_OTHER_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingSubtotalOther, 
-  //   error: errorSubtotalOther, 
-  //   data: dataSubtotalOther,
-  //   refetch: refetchSubtotalOther
-  //  } = useQuery(GET_REVENUE_SUBTOTAL_OTHER_QUERY, {
-  //   variables: { year: year }
-  // })
-
-  // const { 
-  //   loading: loadingTaxOther, 
-  //   error: errorTaxOther, 
-  //   data: dataTaxOther,
-  //   refetch: refetchTaxOther
-  //  } = useQuery(GET_REVENUE_TAX_OTHER_QUERY, {
-  //   variables: { year: year }
-  // })
-
   function refetchData(year) {
-    // Total
     refetch({year: year})
-    // refetchSubtotal({year: year})
-    // refetchTax({year: year})
-    // // Classpasses
-    // refetchTotalPasses({year: year})
-    // refetchSubtotalPasses({year: year})
-    // refetchTaxPasses({year: year})
-    // // Subscritpions
-    // refetchTotalSubs({year: year})
-    // refetchSubtotalSubs({year: year})
-    // refetchTaxSubs({year: year})
-    // // Event tickets
-    // refetchTotalTickets({year: year})
-    // refetchSubtotalTickets({year: year})
-    // refetchTaxTickets({year: year})
-    // // Other
-    // refetchTotalOther({year: year})
-    // refetchSubtotalOther({year: year})
-    // refetchTaxOther({year: year})
   }
-
-  // use <datavar> && <datavar unpacked> as <datavar> might not yet be loaded.
 
   return (
     <InsightRevenueBase year={year} refetchData={refetchData}>
@@ -202,14 +55,12 @@ function InsightRevenue ({ t, history }) {
         data={data && data.insightRevenueEventTickets}
       />
       {/* Other */}
-      {/* <InsightRevenueDisplay
-          loading={(loadingTotalOther || loadingSubtotalOther || loadingTaxOther)}
-          error={(errorTotalOther || errorSubtotalOther || errorTaxOther)}
-          cardTitle={t("general.other")}
-          dataTotal={dataTotalOther && dataTotalOther.insightRevenueTotalOther.data}
-          dataSubtotal={dataSubtotalOther && dataSubtotalOther.insightRevenueSubtotalOther.data}
-          dataTax={dataTaxOther && dataTaxOther.insightRevenueTaxOther.data}
-    /> */}
+      <InsightRevenueDisplay
+        loading={loading}
+        error={error}
+        cardTitle={t("general.other")}
+        data={data && data.insightRevenueOther}
+      />
     </InsightRevenueBase> 
   )
 }
