031c33821f9239bca99c3556223e4ed4d8896123
Edwin van de Ven <edwin@simplycode.nl>
2022-04-14 19:29:06 +0200

    Improve date & time required messages
    More uniform with other messages

--- src/components/finance/taxrates_summary/yupSchema.js
+++ src/components/finance/taxrates_summary/yupSchema.js
@@ -3,9 +3,9 @@ import * as Yup from 'yup'
 
 export const TAX_SUMMARY_SCHEMA = Yup.object().shape({
     dateStart: Yup.date()
-      .typeError(t('yup.please_select_a_date'))
+      .typeError(t('yup.date_required'))
       .required(t('yup.field_required')),
     dateEnd: Yup.date()
-      .typeError(t('yup.please_select_a_date'))
+      .typeError(t('yup.date_required'))
       .required(t('yup.field_required')),
   })
