e4921d9f187f7b7e78419da75be3f14bc218da8c
Edwin van de Ven <edwin@simplycode.nl>
2022-04-14 21:46:58 +0200

    Don't use a pop-up window.open() for downloading files
    Safari blocks it

--- src/tools/refresh_token_and_open_export_link.js
+++ src/tools/refresh_token_and_open_export_link.js
@@ -1,6 +1,18 @@
 import CSLS from './cs_local_storage'
 import { CSAuth } from './authentication';
 
+// Create a link element and simulate a click. This prevents having to redirect creating a pop up that might be blocked.
+function download(dataurl) {
+  let link = document.createElement("a");
+  link.href = dataurl
+  // link.setAttribute('download', '')
+  // Put the link in the dom and click it
+  document.body.appendChild(link);
+  link.click();
+  // Cleanup the DOM
+  document.body.removeChild(link);
+}
+
 export function refreshTokenAndOpenExportLinkInNewTab(doTokenRefresh, history, exportUrl) {  
   const refreshToken = localStorage.getItem(CSLS.AUTH_REFRESH_TOKEN)
 
@@ -11,8 +23,8 @@ export function refreshTokenAndOpenExportLinkInNewTab(doTokenRefresh, history, e
       //Add authentication headers in URL
       var url = `${exportUrl}/${token}`;
 
-      window.open(url, "_blank")
-      // history.push(export_url)
+      // window.open(url, "_blank")
+      download(url);
     }).catch((error) => {
       console.log(error)
       history.push("/#/user/login")
