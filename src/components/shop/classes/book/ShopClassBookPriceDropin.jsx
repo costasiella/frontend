f25d8724559792cf5ca8b72de49c3d2794ccf31f
Edwin van de Ven <edwin@simplycode.nl>
2022-06-22 23:25:50 +0200

    Improve shop class booking options layout

--- src/components/shop/classes/book/ShopClassBookPriceDropin.jsx
+++ src/components/shop/classes/book/ShopClassBookPriceDropin.jsx
@@ -19,7 +19,7 @@ function ShopClassBookPriceDropin({
 }) {
 
   return (
-    <Grid.Col xs={12} sm={12} md={3}>
+    <Grid.Col xs={12} sm={6} md={4} xl={3}>
       <Card 
         statusColor="blue"
         title={t("general.dropin")} >
