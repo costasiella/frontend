1d3cfbbe548520623fc6d74fcc29647cba49995b
Edwin van de Ven <edwin@simplycode.nl>
2022-08-02 08:30:21 +0200

    Add business invoices query

---
+++ src/components/relations/b2b/invoices/queries.js
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client"
+
+export const GET_INVOICES_QUERY = gql`
+  query FinanceInvoices($after: String, $before: String, $status: String, $business: ID) {
+    financeInvoices(first: 15, before: $before, after: $after, status: $status, business: $business) {
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+      edges {
+        node {
+          id
+          account {
+            id
+            fullName
+          }
+          invoiceNumber
+          status
+          summary
+          relationCompany
+          relationContactName
+          dateSent
+          dateDue
+          total
+          totalDisplay
+          balance
+          balanceDisplay
+        }
+      }
+    }
+  }
+`
