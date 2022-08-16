104c1819e927c57911dcd052ffd743090dc644a2
Edwin van de Ven <edwin@simplycode.nl>
2022-08-16 13:38:25 +0200

    Move invoices list to separate component
    For reuse in other places

--- src/components/finance/invoices/FinanceInvoices.jsx
+++ src/components/finance/invoices/FinanceInvoices.jsx
@@ -1,37 +1,21 @@
-// @flow
-
-import React, { useContext } from 'react'
-import { useQuery, useMutation } from "@apollo/client"
-import { v4 } from "uuid"
+import React from 'react'
+import { useQuery } from "@apollo/client"
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-import { Link } from 'react-router-dom'
-
 import {
-  Icon,
   Dimmer,
-  Button,
-  Table, 
-  Text
 } from "tabler-react";
 
-import AppSettingsContext from '../../context/AppSettingsContext'
-import { get_list_query_variables } from "./tools"
+import CSLS from "../../../tools/cs_local_storage"
 import ContentCard from "../../general/ContentCard"
 import FinanceInvoicesBase from "./FinanceInvoicesBase"
-import FinanceInvoicesStatus from "../../ui/FinanceInvoiceStatus"
+import FinanceInvoicesList from './FinanceInvoicesList'
 
-import { GET_INVOICES_QUERY, DELETE_FINANCE_INVOICE } from "./queries"
-
-import CSLS from "../../../tools/cs_local_storage"
-import confirm_delete from "../../../tools/confirm_delete"
-import moment from 'moment'
+import { get_list_query_variables } from "./tools"
+import { GET_INVOICES_QUERY } from "./queries"
 
 
 function FinanceInvoices({ t, location, history }) {
-  const appSettings = useContext(AppSettingsContext)
-  const dateFormat = appSettings.dateFormat
-
   // Set back location for edit invoice
   localStorage.setItem(CSLS.FINANCE_INVOICES_EDIT_RETURN, location.pathname)
   // Fetch data
@@ -39,7 +23,6 @@ function FinanceInvoices({ t, location, history }) {
     variables: get_list_query_variables(),
     fetchPolicy: "network-only"
   })
-  const [ deleteFinanceInvoice ] = useMutation(DELETE_FINANCE_INVOICE)
 
   if (loading) return (
     <FinanceInvoicesBase refetch={refetch}>
@@ -74,116 +57,38 @@ function FinanceInvoices({ t, location, history }) {
 
   return (
     <FinanceInvoicesBase refetch={refetch}>
-      <ContentCard cardTitle={t('finance.invoices.title')}
-                  hasCardBody={false}
-                  pageInfo={invoices.pageInfo}
-                  onLoadMore={() => {
-                    fetchMore({
-                      variables: {
-                        after: invoices.pageInfo.endCursor
-                      },
-                      updateQuery: (previousResult, { fetchMoreResult }) => {
-                        const newEdges = fetchMoreResult.financeInvoices.edges
-                        const pageInfo = fetchMoreResult.financeInvoices.pageInfo
+      <ContentCard 
+        cardTitle={t('finance.invoices.title')}
+        hasCardBody={false}
+        pageInfo={invoices.pageInfo}
+        onLoadMore={() => {
+          fetchMore({
+            variables: {
+              after: invoices.pageInfo.endCursor
+            },
+            updateQuery: (previousResult, { fetchMoreResult }) => {
+              const newEdges = fetchMoreResult.financeInvoices.edges
+              const pageInfo = fetchMoreResult.financeInvoices.pageInfo
 
-                        return newEdges.length
-                          ? {
-                              // Put the new invoices at the end of the list and update `pageInfo`
-                              // so we have the new `endCursor` and `hasNextPage` values
-                              financeInvoices: {
-                                __typename: previousResult.financeInvoices.__typename,
-                                edges: [ ...previousResult.financeInvoices.edges, ...newEdges ],
-                                pageInfo
-                              }
-                            }
-                          : previousResult
-                      }
-                    })
-                  }} 
-        >
-        <Table cards>
-          <Table.Header>
-            <Table.Row key={v4()}>
-              <Table.ColHeader>{t('general.status')}</Table.ColHeader>
-              <Table.ColHeader>{t('finance.invoices.invoice_number')} & {t('finance.invoices.summary')}</Table.ColHeader>
-              <Table.ColHeader>{t('finance.invoices.relation')}</Table.ColHeader>
-              <Table.ColHeader>{t('finance.invoices.date')} & {t('finance.invoices.due')}</Table.ColHeader>
-              {/* <Table.ColHeader>{t('finance.invoices.due')}</Table.ColHeader> */}
-              <Table.ColHeader>{t('general.total')}</Table.ColHeader>
-              <Table.ColHeader>{t('general.balance')}</Table.ColHeader>
-              <Table.ColHeader></Table.ColHeader>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-              {invoices.edges.map(({ node }) => (
-                <Table.Row key={v4()}>
-                  <Table.Col key={v4()}>
-                    <FinanceInvoicesStatus status={node.status} />
-                  </Table.Col>
-                  <Table.Col key={v4()}>
-                    <Link to={"/finance/invoices/edit/" + node.id}>
-                      {node.invoiceNumber}
-                    </Link><br />
-                    <Text.Small color="gray">{node.summary.trunc(28)}</Text.Small>
-                  </Table.Col>
-                  <Table.Col key={v4()}>
-                    {(node.account) && 
-                      <Link to={"/relations/accounts/" + node.account.id + "/profile"}>
-                        <Icon name="user" /> {node.account.fullName} <br />
-                      </Link> }
-                    {(node.business) && 
-                      <Link to={"/relations/b2b/" + node.business.id + "/edit"}>
-                        <small><Icon name="home" /> {node.business.name}</small>
-                      </Link>
+              return newEdges.length
+                ? {
+                    // Put the new invoices at the end of the list and update `pageInfo`
+                    // so we have the new `endCursor` and `hasNextPage` values
+                    financeInvoices: {
+                      __typename: previousResult.financeInvoices.__typename,
+                      edges: [ ...previousResult.financeInvoices.edges, ...newEdges ],
+                      pageInfo
                     }
-                    <br />
-                  </Table.Col>
-                  <Table.Col key={v4()}>
-                    {moment(node.dateSent).format(dateFormat)} <br />
-                    {moment(node.dateDue).format(dateFormat)}
-                  </Table.Col>
-                  <Table.Col key={v4()}>
-                    {node.totalDisplay}
-                  </Table.Col>
-                  <Table.Col key={v4()}>
-                    {node.balanceDisplay}
-                  </Table.Col>
-                  <Table.Col className="text-right" key={v4()}>
-                    <Link to={"/finance/invoices/edit/" + node.id}>
-                      <Button className='btn-sm' 
-                              color="secondary">
-                        {t('general.edit')}
-                      </Button>
-                    </Link>
-                    <button className="icon btn btn-link btn-sm" 
-                      title={t('general.delete')} 
-                      href=""
-                      onClick={() => {
-                        confirm_delete({
-                          t: t,
-                          msgConfirm: t("finance.invoices.delete_confirm_msg"),
-                          msgDescription: <p>{node.invoiceNumber}</p>,
-                          msgSuccess: t('finance.invoices.deleted'),
-                          deleteFunction: deleteFinanceInvoice,
-                          functionVariables: { 
-                            variables: {
-                              input: {
-                                id: node.id
-                              }
-                            }, 
-                            refetchQueries: [
-                              {query: GET_INVOICES_QUERY, variables: get_list_query_variables() } 
-                            ]
-                          }
-                        })
-                    }}>
-                      <span className="text-red"><Icon prefix="fe" name="trash-2" /></span>
-                    </button>
-                </Table.Col>
-                </Table.Row>
-              ))}
-          </Table.Body>
-        </Table>
+                  }
+                : previousResult
+            }
+          })
+        }} 
+      >
+        <FinanceInvoicesList 
+          invoices={invoices} 
+          showColRelation={true}
+        />
       </ContentCard>
     </FinanceInvoicesBase>
   )
---
+++ src/components/finance/invoices/FinanceInvoicesList.jsx
@@ -0,0 +1,126 @@
+import React, { useContext } from 'react'
+import { useQuery, useMutation } from "@apollo/client"
+import { v4 } from "uuid"
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Link } from 'react-router-dom'
+
+import {
+  Icon,
+  Dimmer,
+  Button,
+  Table, 
+  Text
+} from "tabler-react";
+
+import AppSettingsContext from '../../context/AppSettingsContext'
+import { get_list_query_variables } from "./tools"
+import FinanceInvoicesStatus from "../../ui/FinanceInvoiceStatus"
+
+import { GET_INVOICES_QUERY, DELETE_FINANCE_INVOICE } from "./queries"
+
+import CSLS from "../../../tools/cs_local_storage"
+import confirm_delete from "../../../tools/confirm_delete"
+import moment from 'moment'
+
+function FinanceInvoicesList({t, history, match, invoices, showColRelation=false}) {
+  const appSettings = useContext(AppSettingsContext)
+  const dateFormat = appSettings.dateFormat
+
+  const [ deleteFinanceInvoice ] = useMutation(DELETE_FINANCE_INVOICE)
+
+  return (
+    <Table cards>
+        <Table.Header>
+          <Table.Row key={v4()}>
+            <Table.ColHeader>{t('general.status')}</Table.ColHeader>
+            <Table.ColHeader>{t('finance.invoices.invoice_number')} & {t('finance.invoices.summary')}</Table.ColHeader>
+            {showColRelation && <Table.ColHeader>{t('finance.invoices.relation')}</Table.ColHeader>}
+            <Table.ColHeader>{t('finance.invoices.date')} & {t('finance.invoices.due')}</Table.ColHeader>
+            {/* <Table.ColHeader>{t('finance.invoices.due')}</Table.ColHeader> */}
+            <Table.ColHeader>{t('general.total')}</Table.ColHeader>
+            <Table.ColHeader>{t('general.balance')}</Table.ColHeader>
+            <Table.ColHeader></Table.ColHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {invoices.edges.map(({ node }) => (
+            <Table.Row key={v4()}>
+              <Table.Col key={v4()}>
+                <FinanceInvoicesStatus status={node.status} /> <br />
+                {(node.business && !showColRelation) && 
+                  <Link to={"/relations/b2b/" + node.business.id + "/edit"}>
+                    <small><Icon name="home" /> {node.business.name}</small>
+                  </Link>
+                }
+              </Table.Col>
+              <Table.Col key={v4()}>
+                <Link to={"/finance/invoices/edit/" + node.id}>
+                  {node.invoiceNumber}
+                </Link><br />
+                <Text.Small color="gray">{node.summary.trunc(28)}</Text.Small>
+              </Table.Col>
+              {showColRelation &&
+                <Table.Col key={v4()}>
+                  { node.account && 
+                    <Link to={"/relations/accounts/" + node.account.id + "/profile"}>
+                      <Icon name="user" /> {node.account.fullName} <br />
+                    </Link> 
+                  }
+                  { node.business && 
+                    <Link to={"/relations/b2b/" + node.business.id + "/edit"}>
+                      <small><Icon name="home" /> {node.business.name}</small>
+                    </Link>
+                  }
+                </Table.Col>
+              }
+              <Table.Col key={v4()}>
+                {moment(node.dateSent).format(dateFormat)} <br />
+                {moment(node.dateDue).format(dateFormat)}
+              </Table.Col>
+              <Table.Col key={v4()}>
+                {node.totalDisplay}
+              </Table.Col>
+              <Table.Col key={v4()}>
+                {node.balanceDisplay}
+              </Table.Col>
+              <Table.Col className="text-right" key={v4()}>
+                <Link to={"/finance/invoices/edit/" + node.id}>
+                  <Button className='btn-sm' 
+                          color="secondary">
+                    {t('general.edit')}
+                  </Button>
+                </Link>
+                <button className="icon btn btn-link btn-sm" 
+                  title={t('general.delete')} 
+                  href=""
+                  onClick={() => {
+                    confirm_delete({
+                      t: t,
+                      msgConfirm: t("finance.invoices.delete_confirm_msg"),
+                      msgDescription: <p>{node.invoiceNumber}</p>,
+                      msgSuccess: t('finance.invoices.deleted'),
+                      deleteFunction: deleteFinanceInvoice,
+                      functionVariables: { 
+                        variables: {
+                          input: {
+                            id: node.id
+                          }
+                        }, 
+                        refetchQueries: [
+                          {query: GET_INVOICES_QUERY, variables: get_list_query_variables() } 
+                        ]
+                      }
+                    })
+                }}>
+                  <span className="text-red"><Icon prefix="fe" name="trash-2" /></span>
+                </button>
+            </Table.Col>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  )
+}
+
+export default withTranslation()(withRouter(FinanceInvoicesList))
