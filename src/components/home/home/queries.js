e09cea3246b027ed9b6c0848da3d4d69a8a12273
Edwin van de Ven <edwin@simplycode.nl>
2022-08-17 23:09:47 +0200

    Add query for upcoming classes

--- src/components/home/home/queries.js
+++ src/components/home/home/queries.js
@@ -24,3 +24,68 @@ export const GET_BACKEND_ANNOUNCEMENTS_QUERY = gql`
     }
   }
 `
+
+export const GET_BACKEND_INSTRUCTOR_UPCOMING_CLASSES_QUERY = gql`
+query ScheduleClasses(
+  $dateFrom: Date!, 
+  $dateUntil:Date!, 
+  $orderBy: String, 
+  $instructor: ID!
+  $attendanceCountType: String!
+){
+  scheduleClasses(
+      dateFrom:$dateFrom, 
+      dateUntil: $dateUntil, 
+      orderBy: $orderBy, 
+      instructor: $instructor,
+      attendanceCountType: $attendanceCountType,
+      publicOnly: false
+  ){
+    date
+    bookingOpenOn
+    classes {
+      scheduleItemId
+      frequencyType
+      date
+      status
+      holiday
+      holidayName
+      description
+      account {
+        id
+        fullName
+      }
+      role
+      account2 {
+        id
+        fullName
+      }
+      role2
+      organizationLocationRoom {
+        id
+        name
+        organizationLocation {
+          id
+          name
+        }
+      }
+      organizationClasstype {
+        id
+        name
+      }
+      organizationLevel {
+        id
+        name
+      }
+      timeStart
+      timeEnd
+      spaces
+      countAttendance
+      availableSpacesOnline
+      availableSpacesTotal
+      displayPublic
+      bookingStatus
+    }
+  }
+}
+`
