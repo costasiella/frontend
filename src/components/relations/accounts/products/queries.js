2e6e1329437c7a5828e30b9fece46341c6fca871
Edwin van de Ven <edwin@simplycode.nl>
2022-09-26 15:50:54 +0200

    Add AccountProducts list

---
+++ src/components/relations/accounts/products/queries.js
@@ -0,0 +1,160 @@
+import { gql } from "@apollo/client"
+
+export const GET_ACCOUNT_PRODUCTS_QUERY = gql`
+  query AccountProducts($after: String, $before: String, $accountId: ID!) {
+    accountProducts(first: 15, before: $before, after: $after, account: $accountId) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          organizationProduct {
+            id
+            name
+            description
+          }
+          quantity
+          createdAt
+        }
+      }
+    }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
+      urlImageThumbnailSmall
+    }
+  }
+`
+
+export const GET_ACCOUNT_PRODUCT_QUERY = gql`
+  query AccountProduct($id: ID!, $accountId: ID!, $after: String, $before: String) {
+    accountProduct(id:$id) {
+      id
+      organizationProduct {
+        id
+        name
+      }
+      quantity
+      createdAt
+    }
+    organizationClasspasses(first: 100, before: $before, after: $after, archived: false) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+        }
+      }
+    }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
+      urlImageThumbnailSmall
+    }
+  }
+`
+
+export const GET_INPUT_VALUES_QUERY = gql`
+  query ProductInputValues($after: String, $before: String, $accountId: ID!) {
+    organizationProducts(first: 100, before: $before, after: $after, archived: false) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          archived
+          name
+          description
+        }
+      }
+    }
+    account(id:$accountId) {
+      id
+      firstName
+      lastName
+      email
+      phone
+      mobile
+      isActive
+      urlImageThumbnailSmall
+    }
+  }
+`
+
+export const CREATE_ACCOUNT_CLASSPASS = gql`
+  mutation CreateAccountClasspass($input: CreateAccountClasspassInput!) {
+    createAccountClasspass(input: $input) {
+      accountClasspass {
+        id
+        account {
+          id
+          firstName
+          lastName
+          email
+        }
+        organizationClasspass {
+          id
+          name
+        }
+        dateStart
+        dateEnd
+        note
+      }
+    }
+  }
+`
+
+export const UPDATE_ACCOUNT_CLASSPASS = gql`
+mutation UpdateAccountClasspass($input: UpdateAccountClasspassInput!) {
+  updateAccountClasspass(input: $input) {
+    accountClasspass {
+      id
+      account {
+        id
+        firstName
+        lastName
+        email
+      }
+      organizationClasspass {
+        id
+        name
+      }
+      dateStart
+      dateEnd
+      note
+    }
+  }
+}
+`
+
+export const DELETE_ACCOUNT_PRODUCT = gql`
+  mutation DeleteAccountProduct($input: DeleteAccountProductInput!) {
+    deleteAccountProduct(input: $input) {
+      ok
+    }
+  }
+`
