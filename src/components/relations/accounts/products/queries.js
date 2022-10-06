df5aa714d3a61745591f4d80b3d3aa7cc13e7766
Edwin van de Ven <edwin@simplycode.nl>
2022-10-06 13:40:45 +0200

    Remove not implemented queries

--- src/components/relations/accounts/products/queries.js
+++ src/components/relations/accounts/products/queries.js
@@ -46,44 +46,44 @@ export const GET_ACCOUNT_PRODUCTS_QUERY = gql`
   }
 `
 
-export const GET_ACCOUNT_PRODUCT_QUERY = gql`
-  query AccountProduct($id: ID!, $accountId: ID!, $after: String, $before: String) {
-    accountProduct(id:$id) {
-      id
-      organizationProduct {
-        id
-        name
-      }
-      quantity
-      createdAt
-    }
-    organizationClasspasses(first: 100, before: $before, after: $after, archived: false) {
-      pageInfo {
-        startCursor
-        endCursor
-        hasNextPage
-        hasPreviousPage
-      }
-      edges {
-        node {
-          id
-          archived
-          name
-        }
-      }
-    }
-    account(id:$accountId) {
-      id
-      firstName
-      lastName
-      email
-      phone
-      mobile
-      isActive
-      urlImageThumbnailSmall
-    }
-  }
-`
+// export const GET_ACCOUNT_PRODUCT_QUERY = gql`
+//   query AccountProduct($id: ID!, $accountId: ID!, $after: String, $before: String) {
+//     accountProduct(id:$id) {
+//       id
+//       organizationProduct {
+//         id
+//         name
+//       }
+//       quantity
+//       createdAt
+//     }
+//     organizationClasspasses(first: 100, before: $before, after: $after, archived: false) {
+//       pageInfo {
+//         startCursor
+//         endCursor
+//         hasNextPage
+//         hasPreviousPage
+//       }
+//       edges {
+//         node {
+//           id
+//           archived
+//           name
+//         }
+//       }
+//     }
+//     account(id:$accountId) {
+//       id
+//       firstName
+//       lastName
+//       email
+//       phone
+//       mobile
+//       isActive
+//       urlImageThumbnailSmall
+//     }
+//   }
+// `
 
 export const GET_INPUT_VALUES_QUERY = gql`
   query ProductInputValues($after: String, $before: String, $accountId: ID!) {
@@ -125,29 +125,6 @@ export const CREATE_ACCOUNT_PRODUCT = gql`
       }
     }
   }
-`
-
-export const UPDATE_ACCOUNT_CLASSPASS = gql`
-mutation UpdateAccountClasspass($input: UpdateAccountClasspassInput!) {
-  updateAccountClasspass(input: $input) {
-    accountClasspass {
-      id
-      account {
-        id
-        firstName
-        lastName
-        email
-      }
-      organizationClasspass {
-        id
-        name
-      }
-      dateStart
-      dateEnd
-      note
-    }
-  }
-}
 `
 
 export const DELETE_ACCOUNT_PRODUCT = gql`
