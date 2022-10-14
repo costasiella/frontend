154c4fedfb431e2bf458f7347ccb6cb5341ce17b
Edwin van de Ven <edwin@simplycode.nl>
2022-10-14 19:01:17 +0200

    Add AccountQuotesBase component

---
+++ src/components/relations/accounts/quotes/AccountQuotesBase.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import {
+  Page,
+  Grid,
+  Container,
+} from "tabler-react";
+import SiteWrapper from "../../../SiteWrapper"
+
+import ProfileMenu from "../ProfileMenu"
+import ProfileCardSmall from "../../../ui/ProfileCardSmall"
+
+function AccountQuotesBase({ t, match, history, children, account={}, pageHeaderButtonList="" }) {
+  const accountId = match.params.account_id
+  let pageHeader
+  if (account) {
+    pageHeader = account.fullName
+  }
+
+  return (
+    <SiteWrapper>
+      <div className="my-3 my-md-5">
+        <Container>
+          <Page.Header title={pageHeader} >
+            <div className='page-options d-flex'>
+              { pageHeaderButtonList }
+            </div>
+          </Page.Header>
+          <Grid.Row>
+            <Grid.Col md={9}>
+              {children}
+            </Grid.Col>
+            <Grid.Col md={3}>
+              <ProfileCardSmall user={account}/>
+              <ProfileMenu 
+                activeLink='quotes' 
+                accountId={accountId}
+              />
+            </Grid.Col>
+          </Grid.Row>
+        </Container>
+      </div>
+    </SiteWrapper>
+  )
+}
+
+
+export default withTranslation()(withRouter(AccountQuotesBase))
