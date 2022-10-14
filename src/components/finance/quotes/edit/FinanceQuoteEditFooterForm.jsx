02ff1b330f2ccb67e41db0d1e55b3f6e39bfd7b2
Edwin van de Ven <edwin@simplycode.nl>
2022-10-14 19:55:02 +0200

    Add FinanceQuoteEditFooterForm component

---
+++ src/components/finance/quotes/edit/FinanceQuoteEditFooterForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+import { Form as FoForm, ErrorMessage } from 'formik'
+
+import {
+  Button,
+  Dimmer,
+  Form,
+} from "tabler-react"
+
+import { Editor } from '@tinymce/tinymce-react'
+import { tinymceBasicConf } from "../../../../plugin_config/tinymce"
+
+// Use editor as controlled component:
+// https://github.com/tinymce/tinymce-react/blob/master/README.md
+
+const FinanceQuoteEditFooterForm = ({ t, isSubmitting, values, errors, touched, handleChange, setFieldTouched, setFieldValue }) => (
+  <Dimmer loader={isSubmitting} active={isSubmitting}>
+    <FoForm>
+      <Form.Group label={t('general.footer')}>
+        <Editor
+          tinymceScriptSrc="/d/static/tinymce/tinymce.min.js"
+          textareaName="footer"
+          initialValue={values.footer}
+          init={tinymceBasicConf}
+          onBlur={(e) => {
+            setFieldValue("footer", e.target.getContent())
+            setFieldTouched("footer", true, true)
+          }}
+        />
+        <ErrorMessage name="footer" component="span" className="invalid-feedback" />
+      </Form.Group>
+      <Button 
+        color="primary"
+        className="pull-right" 
+        type="submit" 
+        disabled={isSubmitting}
+      >
+        {t('general.submit')}
+      </Button>
+    </FoForm>
+  </Dimmer>
+)
+
+export default withTranslation()(withRouter(FinanceQuoteEditFooterForm))
