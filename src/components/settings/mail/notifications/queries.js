ae9c80cb6b4430799ad63105fd37dd8d923e7eb0
Edwin van de Ven <edwin@simplycode.nl>
2022-10-17 21:20:05 +0200

    Add notifications query

---
+++ src/components/settings/mail/notifications/queries.js
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client"
+
+export const GET_NOTIFICATIONS_QUERY = gql`
+  query systemNotifications($after: String, $before: String) {
+    systemNotifications(first: 15, before: $before, after: $after) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        node {
+          id
+          name
+          accounts {
+            id
+            fullName
+            email
+          }
+        }
+      }
+    }
+  }
+`
