45d1223bc03ed67b654cf9651e2078541e1b7986
Edwin van de Ven <edwin@simplycode.nl>
2022-11-18 12:01:04 +0100

    Add AccountEnrollmentsBase component

---
+++ src/components/relations/accounts/enrollments/AccountEnrollmentsBase.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import {
+  Page,
+  Grid,
+  Container
+} from "tabler-react";
+import SiteWrapper from "../../../SiteWrapper"
+import RelationsAccountsBack from "../RelationsAccountsBack"
+
+import ProfileMenu from "../ProfileMenu"
+import ProfileCardSmall from "../../../ui/ProfileCardSmall"
+
+
+function AccountEnrollmentsBase({ t, match, history, children, account={} }) {
+  const accountId = match.params.account_id
+
+  return (
+    <SiteWrapper>
+      <div className="my-3 my-md-5">
+        <Container>
+          <Page.Header title={account.fullName} >
+            <div className="page-options d-flex">
+              <RelationsAccountsBack />
+            </div>
+          </Page.Header>
+          <Grid.Row>
+            <Grid.Col md={9}>
+              {children}
+            </Grid.Col>
+            <Grid.Col md={3}>
+              <ProfileCardSmall user={account}/>
+              <ProfileMenu 
+                activeLink='enrollments' 
+                accountId={accountId}
+              />
+            </Grid.Col>
+          </Grid.Row>
+        </Container>
+      </div>
+    </SiteWrapper>
+  )
+}
+
+export default withTranslation()(withRouter(AccountEnrollmentsBase))
