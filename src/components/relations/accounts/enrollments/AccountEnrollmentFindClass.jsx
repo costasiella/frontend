0f1052daae9c330efe591fb387d3f15f4a8a4741
Edwin van de Ven <edwin@simplycode.nl>
2022-11-24 13:02:11 +0100

    Show loader & error message with layout in find class component

--- src/components/relations/accounts/enrollments/AccountEnrollmentFindClass.jsx
+++ src/components/relations/accounts/enrollments/AccountEnrollmentFindClass.jsx
@@ -1,59 +1,14 @@
 import React from 'react'
-import { useQuery, useMutation } from '@apollo/client'
-import { v4 } from "uuid"
+import { useQuery } from '@apollo/client'
 import { withTranslation } from 'react-i18next'
 import { withRouter } from "react-router"
-
-import {
-  Alert,
-  Button,
-  Card,
-  Dimmer,
-  Dropdown,
-  Icon,
-  Table
-} from "tabler-react";
-import HasPermissionWrapper from "../../../HasPermissionWrapper"
-import { toast } from 'react-toastify'
-// import { get_attendance_list_query_variables } from "./tools"
+import { Dimmer } from 'tabler-react';
 
 import { GET_ACCOUNT } from '../../../../queries/accounts/get_account'
 
-// import ContentCard from "../../../../general/ContentCard"
-// import ButtonConfirm from '../../../../ui/ButtonConfirm'
 import SearchClassesOnDate from '../../../ui/SearchClassesOnDate'
 import AccountEnrollmentsBase from "./AccountEnrollmentsBase"
 import AccountEnrollmentBack from './AccountEnrollmentBack'
-
-// function setAttendanceStatus({t, match, updateAttendance, node, status}) {
-//   const schedule_item_id = match.params.class_id
-//   const class_date = match.params.date
-
-//   updateAttendance({
-//     variables: { 
-//       input: {
-//         id: node.id, 
-//         bookingStatus: status
-//       }
-//     },
-//     refetchQueries: [
-//       {query: GET_SCHEDULE_CLASS_ATTENDANCE_QUERY, 
-//         variables: get_attendance_list_query_variables(schedule_item_id, class_date)}
-//     ]
-//   }).then(({ data }) => {
-//     console.log('got data', data);
-//     toast.success(
-//       t('schedule.classes.class.attendance.status_saved'), {
-//         position: toast.POSITION.BOTTOM_RIGHT
-//       })
-//   }).catch((error) => {
-//     toast.error((t('general.toast_server_error')) +  error, {
-//         position: toast.POSITION.BOTTOM_RIGHT
-//       })
-//     console.log('there was an error sending the query', error);
-//   })
-// }
-
 
 function AccountEnrollmentFindClass({ t, match, location }) {
   const accountId = match.params.account_id
@@ -61,11 +16,19 @@ function AccountEnrollmentFindClass({ t, match, location }) {
   const { loading, error, data } = useQuery(GET_ACCOUNT, { variables: { accountId: accountId }})
 
   if (loading) { 
-    return "loading"
+    return (
+      <AccountEnrollmentsBase pageHeaderButtonList={pageHeaderButtonList} >
+        <Dimmer active={true} loader={true} />
+      </AccountEnrollmentsBase>
+    )
   }
 
   if (error) { 
-    return "error"
+    return (
+      <AccountEnrollmentsBase pageHeaderButtonList={pageHeaderButtonList} >
+        <p>{t('general.error_sad_smiley')}</p>
+      </AccountEnrollmentsBase>
+    )
   }
 
   const account = data.account
