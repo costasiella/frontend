51116f0f87c275b4aa2f689f21cec7eeb1e671b7
Edwin van de Ven <edwin@simplycode.nl>
2023-01-13 20:11:46 +0100

    Display unlimited credits in backend booking options
    If unlimited is set in the organization subscription

--- src/components/schedule/classes/class/book/queries.js
+++ src/components/schedule/classes/class/book/queries.js
@@ -73,6 +73,7 @@ export const GET_BOOKING_OPTIONS_QUERY = gql`
           organizationSubscription {
             id
             name
+            unlimited
           }
           creditTotal
         }
