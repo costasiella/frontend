714e42b0522bfd0109fdaa60869568a8b7d2833a
Edwin van de Ven <edwin@simplycode.nl>
2023-03-02 20:38:15 +0100

    Add InsightInstructorClassesMonthBase component

---
+++ src/components/insight/instructor_classes/InsightInstructorClassesMonthBase.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { withTranslation } from 'react-i18next'
+import { withRouter } from "react-router"
+
+import {
+  Container,
+  Page
+} from "tabler-react"
+
+import SiteWrapper from '../../../SiteWrapper'
+import InsightBackHome from '../InsightBackHome'
+
+
+function InsightInstructorClassesMonthBase({t, history, children}) {
+
+  return (
+    <SiteWrapper>
+      <div className="my-3 my-md-5">
+        <Container>
+          <Page.Header title={t("finance.title")}>
+            <div className="page-options d-flex">
+              <InsightBackHome />
+            </div>
+          </Page.Header>
+          {children}
+        </Container>
+      </div>
+    </SiteWrapper>
+  )
+}
+
+export default withTranslation()(withRouter(InsightInstructorClassesMonthBase))
