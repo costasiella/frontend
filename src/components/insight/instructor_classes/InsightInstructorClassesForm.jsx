ff71f461e67b97dbd83c7a1707006f81c0bd02a8
Edwin van de Ven <edwin@simplycode.nl>
2023-03-06 21:16:00 +0100

    Fix month display.in InsightInstructurClassesForm

--- src/components/insight/instructor_classes/InsightInstructorClassesForm.jsx
+++ src/components/insight/instructor_classes/InsightInstructorClassesForm.jsx
@@ -31,15 +31,15 @@ const InsightInstructorClassesForm = ({ t, history, inputData, isSubmitting, set
                     className={(errors.month) ? "form-control is-invalid" : "form-control"} 
                     autoComplete="off"
               >
-                <option value="01" key={v4()}>{t("datetime.months.january")}</option>
-                <option value="02" key={v4()}>{t("datetime.months.february")}</option>
-                <option value="03" key={v4()}>{t("datetime.months.march")}</option>
-                <option value="04" key={v4()}>{t("datetime.months.april")}</option>
-                <option value="05" key={v4()}>{t("datetime.months.may")}</option>
-                <option value="06" key={v4()}>{t("datetime.months.june")}</option>
-                <option value="07" key={v4()}>{t("datetime.months.july")}</option>
-                <option value="08" key={v4()}>{t("datetime.months.august")}</option>
-                <option value="09" key={v4()}>{t("datetime.months.september")}</option>
+                <option value="1" key={v4()}>{t("datetime.months.january")}</option>
+                <option value="2" key={v4()}>{t("datetime.months.february")}</option>
+                <option value="3" key={v4()}>{t("datetime.months.march")}</option>
+                <option value="4" key={v4()}>{t("datetime.months.april")}</option>
+                <option value="5" key={v4()}>{t("datetime.months.may")}</option>
+                <option value="6" key={v4()}>{t("datetime.months.june")}</option>
+                <option value="7" key={v4()}>{t("datetime.months.july")}</option>
+                <option value="8" key={v4()}>{t("datetime.months.august")}</option>
+                <option value="9" key={v4()}>{t("datetime.months.september")}</option>
                 <option value="10" key={v4()}>{t("datetime.months.october")}</option>
                 <option value="11" key={v4()}>{t("datetime.months.november")}</option>
                 <option value="12" key={v4()}>{t("datetime.months.december")}</option>
