bff7d60738e3b8a4a373436785c1ec57972da582
Edwin van de Ven <edwin@simplycode.nl>
2023-05-29 21:27:36 +0200

    Add date filter to finance expenses

--- src/components/finance/expenses/FinanceExpensesFilter.jsx
+++ src/components/finance/expenses/FinanceExpensesFilter.jsx
@@ -23,11 +23,19 @@ function getDefaultValue(lsKey) {
 }
 
 function getDefaultValueDate(lsKey) {
-  let dateValue = new Date()
+  let dateValue
+  if (lsKey == CSLS.FINANCE_EXPENSES_FILTER_DATE_FROM) {
+    dateValue = new Date(moment().startOf('quarter'))  
+  } else {
+    dateValue = new Date(moment().endOf('quarter'))  
+  }
+  
   const lsValue = localStorage.getItem(lsKey)
   if (lsValue) {
     dateValue = new Date(lsValue)
   }
+
+  // TODO: Add a sanity check here. Date until can't be smaller then date from
 
   return dateValue
 }
@@ -43,33 +51,15 @@ const selectClass = "form-control custom-select mb-2"
 function FinanceExpensesFilter({ t, history, data, refetch }) {
   let [supplier, setSupplier] = useState(getDefaultValue(CSLS.FINANCE_EXPENSES_FILTER_SUPPLIER))
   let [dateFrom, setDateFrom] = useState(getDefaultValueDate(CSLS.FINANCE_EXPENSES_FILTER_DATE_FROM))
-
-
+  let [dateUntil, setDateUntil] = useState(getDefaultValueDate(CSLS.FINANCE_EXPENSES_FILTER_DATE_UNTIL))
 
   return (
     <div className="mb-4">
-      <Grid.Row>
-        {/* Reset filter button */}
-        <Grid.Col md={12}>
-          <Button
-            className="float-right"
-            color="link"
-            size="sm"
-            onClick={() => {
-              localStorage.setItem(CSLS.FINANCE_EXPENSES_FILTER_SUPPLIER, "")
-              setSupplier("")
-              refetch(get_list_query_variables())
-            }}
-          >
-            {t("general.reset_filter")}
-          </Button>
-          <h6 className="mt-2 pt-1">{t("general.filter")}</h6>
-        </Grid.Col>
-      </Grid.Row>
       {/* Filter */}
       <Grid.Row>
         <Grid.Col lg={3} md={4} s={6} xs={12}>
-          {/* Locations */}
+          {/* Supplier */}
+          <label class="bold">{t("finance.expenses.filter_label_supplier")}</label>
           <select 
             className={selectClass}
             value={supplier}
@@ -89,6 +79,7 @@ function FinanceExpensesFilter({ t, history, data, refetch }) {
           </select>
         </Grid.Col>
         <Grid.Col lg={3} md={4} s={6} xs={12}>
+          <label class="bold">{t("finance.expenses.filter_label_date_from")}</label>
           <CSDatePicker 
             className="form-control mr-2"
             selected={dateFrom}
@@ -102,25 +93,41 @@ function FinanceExpensesFilter({ t, history, data, refetch }) {
                 refetch
               )
             }}
-            placeholderText={t('finance.expenses.filter_date_from')}
+            placeholderText={t('finance.expenses.filter_label_date_from')}
           />
         </Grid.Col>
-        {/* <Grid.Col lg={3} md={4} s={6} xs={12}>
+        <Grid.Col lg={3} md={4} s={6} xs={12}>
+          <label class="bold">{t("finance.expenses.filter_label_date_until")}</label>
           <CSDatePicker 
             className="form-control mr-2"
-            selected={new Date(localStorage.getItem(CSLS.FINANCE_EXPENSES_FILTER_DATE_UNTIL))}
-            isClearable={true}
+            selected={dateUntil}
+            isClearable={false}
             onChange={(date) => {
-              let dateUntil = moment(date).format('YYYY-MM-DD')
+              let dateStr = moment(date).format('YYYY-MM-DD')
+              setDateUntil(date)
               updateLocalStorageAndRefetch(
                 CSLS.FINANCE_EXPENSES_FILTER_DATE_UNTIL,
-                dateUntil,
+                dateStr,
                 refetch
               )
             }}
-            placeholderText={t('finance.expenses.filter_date_until')}
+            placeholderText={t('finance.expenses.filter_label_date_until')}
           />
-        </Grid.Col> */}
+        </Grid.Col>
+        <Grid.Col lg={3} md={4} s={6} xs={12}>
+          <Button
+            className="float-right"
+            color="link"
+            size="sm"
+            onClick={() => {
+              localStorage.setItem(CSLS.FINANCE_EXPENSES_FILTER_SUPPLIER, "")
+              setSupplier("")
+              refetch(get_list_query_variables())
+            }}
+          >
+            {t("general.reset_filter")}
+          </Button>
+        </Grid.Col>
       </Grid.Row>
     </div>
   )
